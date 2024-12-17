0b4948183fb4c4963e33b66c9f86776e74ff642d
lwz <905328134@qq.com>
2024-12-18 02:51:36 +0800

    initial

--- scripts/v1_5/m_train/finetune_lora_7b_bs4_debug100.sh
+++ scripts/v1_5/m_train/finetune_lora_7b_bs4_debug100.sh
@@ -23,7 +23,7 @@ deepspeed llava/train/train_mem.py \
     --max_steps 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
