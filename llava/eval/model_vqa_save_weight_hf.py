b7ec84e07c331d61a5eda97724de02438bb8c514
lwz <905328134@qq.com>
2024-12-18 05:10:13 +0800

    initial

--- llava/eval/model_vqa_save_weight_hf.py
+++ llava/eval/model_vqa_save_weight_hf.py
@@ -5,6 +5,7 @@ from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
 from transformers import Trainer, TrainingArguments
 
+
 def save_model_with_trainer(args):
     # Disable torch initialization to avoid unnecessary operations
     disable_torch_init()
@@ -18,7 +19,7 @@ def save_model_with_trainer(args):
     training_args = TrainingArguments(
         output_dir=os.path.expanduser(args.save_path),  # Directory to save the model and tokenizer
         do_train=False,  # No training
-        do_eval=False,   # No evaluation
+        do_eval=False,  # No evaluation
         logging_dir=None,  # No logging required
         save_strategy="no",  # No automatic saving during training
     )
@@ -30,11 +31,11 @@ def save_model_with_trainer(args):
         tokenizer=tokenizer  # Include the tokenizer to save it with the model
     )
 
-
-    trainer.model.generation_config.do_sample=True
+    trainer.model.generation_config.do_sample = True
     # Save the model and tokenizer
     trainer.save_model()
     print(f"Model and tokenizer saved to {os.path.expanduser(args.save_path)}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
