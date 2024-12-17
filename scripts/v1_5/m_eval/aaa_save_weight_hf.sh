e01d516f07c59823c72c8e311a518f8a3ad0c254
lwz <905328134@qq.com>
2024-12-18 05:00:32 +0800

    initial

--- scripts/v1_5/m_eval/aaa_save_weight_hf.sh
+++ scripts/v1_5/m_eval/aaa_save_weight_hf.sh
@@ -5,7 +5,7 @@ export HF_HOME=/ppio_net0/huggingface
 MODEL="llava-v1.5-7b-lora-jt"
 MODEL_BASE="lmsys/vicuna-7b-v1.5"
 # Evaluate the model
-python -m llava.eval.model_vqa_save_weight \
+python -m llava.eval.model_vqa_save_weight_hf \
     --model-path "checkpoints/$MODEL" \
     --model-base $MODEL_BASE \
     --save-path "checkpoints/$MODEL"
