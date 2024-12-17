f0c3542b1d9e075798792c234a2b95786806be0d
lwz <905328134@qq.com>
2024-12-18 01:13:11 +0800

    initial

--- scripts/v1_5/m_train/finetune_lora_7b_bs4_debug100-task2.sh
+++ scripts/v1_5/m_train/finetune_lora_7b_bs4_debug100-task2.sh
@@ -18,7 +18,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-lora-debug \
+    --output_dir ./checkpoints/llava-v1.5-7b-lora-debug-task2 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
