681459159cb4a5a257989efa03dbc18783cf25fd
lwz <905328134@qq.com>
2024-12-18 02:45:21 +0800

    initial

--- llava/train/train.py
+++ llava/train/train.py
@@ -1035,7 +1035,7 @@ def train(attn_implementation=None):
 
         # import pdb; pdb.set_trace()
         trainer.model = trainer.model.merge_and_unload()
-        # trainer.model = trainer.model.base_model.model
+        trainer.model = trainer.model.base_model.model
         safe_save_model_for_hf_trainer(trainer=trainer,output_dir=training_args.output_dir)
     else:
         # if training_args.local_rank == 0 or training_args.local_rank == -1:
