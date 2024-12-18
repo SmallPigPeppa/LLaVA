d9b00a98e55a7894c44efc7d08a31e1e36a83f3e
lwz <905328134@qq.com>
2024-12-19 02:03:24 +0800

    initial

--- scripts/v1_5/m_eval_continual/llavabench_data_merged.sh
+++ scripts/v1_5/m_eval_continual/llavabench_data_merged.sh
@@ -3,7 +3,7 @@ export HF_HOME=/ppio_net0/huggingface
 
 
 # Define task suffixes in a list
-TASKS=("task-task1-merged" "task-task2-merged" "task-task3-merged" "task-task4-merged" "task-task5-merged")
+#TASKS=("task-task1-merged" "task-task2-merged" "task-task3-merged" "task-task4-merged" "task-task5-merged")
 TASKS=("task-task5-merged")
 
 
