65827efc04cec9577bb1b3d179f63502c4071eeb
lwz <905328134@qq.com>
2024-12-20 03:27:11 +0800

    initial

--- scripts/v1_5/m_eval_continual/generate_Aold_multi.sh
+++ scripts/v1_5/m_eval_continual/generate_Aold_multi.sh
@@ -6,10 +6,10 @@ TASK="task-coco-merged"
 
 # Define the input and output files
 input_file="./playground/data/debug/llava_v1_5_mix665k-random-8.json"
-output_file="./playground/data/debug/llava_v1_5_mix665k-random-8_Aold.json"
+output_file="./playground/data/debug/llava_v1_5_mix665k-random-8-Aold.json"
 
-# Define temporary directory for intermediate results
-tmp_dir="./c-llava-cache"
+# Generate temporary directory name based on the output file
+tmp_dir="./c-llava-cache-${output_file##*/}"
 mkdir -p $tmp_dir
 
 # Number of GPUs
@@ -34,7 +34,7 @@ do
     fi
 
     # Use jq to slice the dataset for each GPU part
-    tmp_output_file="$tmp_dir/llava_v1_5_mix665k-random-8_part_$((i + 1)).json"
+    tmp_output_file="$tmp_dir/part_$((i + 1)).json"
     jq ".[$START_IDX:$END_IDX]" $input_file > $tmp_output_file
 done
 
@@ -45,8 +45,8 @@ do
     CUDA_VISIBLE_DEVICES=$i python -m llava.eval.model_vqa_generate_Aold \
         --model-path "continual-ckpt/domain/llava-v1.5-7b-lora-$TASK" \
         --image-folder ./playground/data \
-        --dataset-file "$tmp_dir/llava_v1_5_mix665k-random-8_part_$((i + 1)).json" \
-        --output-file "$tmp_dir/llava_v1_5_mix665k-random-8_Aold_part_$((i + 1)).json" \
+        --dataset-file "$tmp_dir/part_$((i + 1)).json" \
+        --output-file "$tmp_dir/output_part_$((i + 1)).json" \
         --conv-mode llava_v1 &
 done
 
@@ -55,7 +55,7 @@ wait
 
 # Combine all results into the final output file without sorting
 echo "Combining results without sorting using jq..."
-jq -s 'add' $tmp_dir/llava_v1_5_mix665k-random-8_Aold_part_*.json > $output_file
+jq -s 'add' $tmp_dir/output_part_*.json > $output_file
 
 # Clean up part files in tmp_dir
 rm -rf $tmp_dir
