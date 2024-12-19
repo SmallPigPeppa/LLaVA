89be46fc9f89264b78f15a59dff77112105829cd
lwz <905328134@qq.com>
2024-12-19 23:47:16 +0800

    initial

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -257,6 +257,7 @@ class LlavaMetaForCausalLM(ABC):
                 cur_new_input_embeds.append(cur_input_embeds_no_im[i])
                 cur_new_labels.append(cur_labels_noim[i])
                 if i < num_images:
+                    import pdb; pdb.set_trace()
                     cur_image_features = image_features[cur_image_idx]
                     cur_image_idx += 1
                     cur_new_input_embeds.append(cur_image_features)
