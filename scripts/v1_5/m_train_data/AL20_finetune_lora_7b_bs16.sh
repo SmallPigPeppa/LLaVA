22647549ba40eea09b4ce795a4d5308c5f178ba9
lwz <905328134@qq.com>
2024-12-20 15:59:37 +0800

    initial

--- scripts/v1_5/m_train_data/AL20_finetune_lora_7b_bs16.sh
+++ scripts/v1_5/m_train_data/AL20_finetune_lora_7b_bs16.sh
@@ -50,9 +50,9 @@ for i in ${!TASK_NAMES[@]}; do
         --bf16 True \
         --output_dir ${OUTPUT_DIR} \
         --num_train_epochs 1 \
-        --per_device_train_batch_size 4 \
-        --per_device_eval_batch_size 1 \
-        --gradient_accumulation_steps 4 \
+        --per_device_train_batch_size 16 \
+        --per_device_eval_batch_size 4 \
+        --gradient_accumulation_steps 1 \
         --evaluation_strategy \"no\" \
         --save_strategy \"steps\" \
         --save_steps 50000 \
