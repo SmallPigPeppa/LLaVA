bcca2bdc7b52f90bc3678dd267592f72c5b1eef6
lwz <905328134@qq.com>
2024-12-21 22:20:56 +0800

    initial

--- A-mix/run_Amix.sh
+++ A-mix/run_Amix.sh
@@ -1,7 +1,7 @@
 python main.py \
   --api_key dcf59e46-a998-4a69-adfd-31aeabac1760 \
-  --input_file input/llava_v1_5_mix665k-ocr_vqa-Aold-step1-stack-dedupedv2.json \
-  --output_file output/debug.json \
+  --input_file input/part1.json \
+  --output_file output/part1.json \
   --base_url https://api.ppinfra.com/v3/openai \
   --model qwen/qwen2.5-32b-instruct \
   --max_tokens 2048 \
