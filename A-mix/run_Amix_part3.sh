06062365015dc80aa3a5b7b379a0c078b08d7d2f
lwz <905328134@qq.com>
2024-12-22 00:40:05 +0800

    initial

---
+++ A-mix/run_Amix_part3.sh
@@ -0,0 +1,8 @@
+python main.py \
+  --api_key 1jBqtjpMAw9MVm556Rsgvi1ZAhOqb7uq \
+  --input_file input/part2.json \
+  --output_file output/part2.json \
+  --base_url https://api.ppinfra.com/v3/openai \
+  --model qwen/qwen2.5-32b-instruct \
+  --max_tokens 2048 \
+  --max_workers 8
