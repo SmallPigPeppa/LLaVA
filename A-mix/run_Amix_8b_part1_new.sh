baedd4375a937998047bb99e35dab50d7fc2f866
lwz <905328134@qq.com>
2024-12-22 02:44:04 +0800

    initial

--- A-mix/run_Amix_8b_part1_new.sh
+++ A-mix/run_Amix_8b_part1_new.sh
@@ -6,7 +6,7 @@
 # 3) If the total time (execution + sleep) is less than 60 seconds, stop.
 
 MAX_RUNS=1000         # Maximum number of runs
-MIN_TOTAL_TIME=60    # If execution + sleep is less than this, stop early
+MIN_TOTAL_TIME=300    # If execution + sleep is less than this, stop early
 SLEEP_TIME=30        # Sleep time after each run
 
 for ((i=1; i<=MAX_RUNS; i++))
