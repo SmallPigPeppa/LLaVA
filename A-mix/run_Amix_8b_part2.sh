3c6832a058fb0f7effc353a8b5107c2947efddc0
lwz <905328134@qq.com>
2024-12-22 01:47:10 +0800

    initial

---
+++ A-mix/run_Amix_8b_part2.sh
@@ -0,0 +1,8 @@
+python main.py \
+  --api_key 09604121-6e12-4e83-8e2e-277b6450b32c \
+  --input_file input/part2.json \
+  --output_file output-8b/part2.json \
+  --base_url https://api.novita.ai/v3/openai \
+  --model meta-llama/llama-3.1-8b-instruct \
+  --max_tokens 2048 \
+  --max_workers 32
