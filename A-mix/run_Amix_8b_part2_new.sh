9e8fe5e2ef10f1cd92f7a6b2c59ab508472658bf
lwz <905328134@qq.com>
2024-12-22 19:14:13 +0800

    initial

--- A-mix/run_Amix_8b_part2_new.sh
+++ A-mix/run_Amix_8b_part2_new.sh
@@ -18,14 +18,14 @@ do
 
     # ----------------------------
     # Adjust the Python command as needed
-    python main_new.py \
+    python main_new_llama.py \
       --api_key 09604121-6e12-4e83-8e2e-277b6450b32c \
       --input_file input/part2.json \
       --output_file output-8b/part2.json \
       --base_url https://api.novita.ai/v3/openai \
       --model meta-llama/llama-3.1-8b-instruct \
       --max_tokens 2048 \
-      --max_workers 40
+      --max_workers 1
     # ----------------------------
 
     # Record the end time
