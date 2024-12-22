4955d60dbd86387663eeb13330951dfdfe3f49ab
lwz <905328134@qq.com>
2024-12-23 00:27:41 +0800

    initial

--- A-mix/run_Amix_8b_part1_new_qwen_fixbug.sh
+++ A-mix/run_Amix_8b_part1_new_qwen_fixbug.sh
@@ -21,7 +21,7 @@ do
     python main_new_fixbug.py \
       --api_key 7132bb0a-223c-4fd0-9d9e-893d1bcb17b0 \
       --input_file input/part1.json \
-      --output_file output-qwen/part1.json \
+      --output_file output-qwen-fixbug/part1.json \
       --base_url https://api.ppinfra.com/v3/openai \
       --model qwen/qwen2.5-32b-instruct \
       --max_tokens 4096 \
