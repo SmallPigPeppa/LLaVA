70f243520524e6524de3f7d815cfa24c4663e6b7
lwz <905328134@qq.com>
2024-12-23 02:34:45 +0800

    initial

--- scripts/v1_5/m_train_domain_Amix/finetune_lora_7b_bs4_task2_ocr.sh
+++ scripts/v1_5/m_train_domain_Amix/finetune_lora_7b_bs4_task2_ocr.sh
@@ -35,7 +35,7 @@ deepspeed llava/train/train_mem.py \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 5e-5 \
+    --learning_rate 1e-6 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
