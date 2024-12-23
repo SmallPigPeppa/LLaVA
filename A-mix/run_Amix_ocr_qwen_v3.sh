e0483d861cc0350536d3ab49e5f5adeaf74a64ff
lwz <905328134@qq.com>
2024-12-23 12:27:51 +0800

    initial

--- A-mix/run_Amix_ocr_qwen_v3.sh
+++ A-mix/run_Amix_ocr_qwen_v3.sh
@@ -2,7 +2,7 @@
 
     # ----------------------------
     # Adjust the Python command as needed
-python main_new_fixbugv2.py \
+python main_new_fixbugv3.py \
   --api_key 7132bb0a-223c-4fd0-9d9e-893d1bcb17b0 \
   --input_file playground/data/domain-incremental/llava_v1_5_mix665k-ocr_vqa.json \
   --output_file output-qwen-v3/ocr.json \
