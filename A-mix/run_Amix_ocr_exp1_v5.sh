9cef042a5bb187f3dd9fe64ec28e0fec886a9b17
lwz <905328134@qq.com>
2024-12-23 23:48:35 +0800

    initial

---
+++ A-mix/run_Amix_ocr_exp1_v5.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python main_new_fixbugv4.py \
+  --api_key 7132bb0a-223c-4fd0-9d9e-893d1bcb17b0 \
+  --input_file /ppio_net0/code/LLaVA/playground/data/exp1/part2-Aold.json \
+  --output_file /ppio_net0/code/LLaVA/playground/data/exp1/part2-mix.json \
+  --base_url https://api.ppinfra.com/v3/openai \
+  --model qwen/qwen-2.5-72b-instruct \
+  --max_tokens 2048 \
+  --max_workers 100
+
