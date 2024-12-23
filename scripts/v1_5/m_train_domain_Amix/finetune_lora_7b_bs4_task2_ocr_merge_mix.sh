dec6fee55b9ad344c0ea83135b0b5c11ea3520f4
lwz <905328134@qq.com>
2024-12-23 17:16:04 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_Amix/finetune_lora_7b_bs4_task2_ocr_merge_mix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set Hugging Face cache directory
+export HF_HOME=/ppio_net0/huggingface
+# Limit script to only use GPU 0
+#export CUDA_VISIBLE_DEVICES=0
+# Manually specify model and vision configuration
+MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
+MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa"
+Ratio=0.2
+#OUT_PATH=${MODEL_PATH}-merged-ratio${Ratio}
+OUT_PATH="continual-ckpt/domain-mix/llava-v1.5-7b-lora-task-textvqa-merged-ratio${Ratio}"
+
+echo "OCR task training completed successfully!"
+
+python -m llava.eval.model_vqa_save_weight_hf_mix \
+  --model-path ${MODEL_PATH} \
+  --model-base ${MODEL_BASE} \
+  --save-path ${OUT_PATH} \
+  --lora-scale-factor ${Ratio}
