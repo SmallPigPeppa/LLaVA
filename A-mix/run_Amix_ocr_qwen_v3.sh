a0d6b621dfd882005b8bbfac35248622776ead09
lwz <905328134@qq.com>
2024-12-23 12:28:35 +0800

    initial

--- A-mix/run_Amix_ocr_qwen_v3.sh
+++ A-mix/run_Amix_ocr_qwen_v3.sh
@@ -4,7 +4,7 @@
     # Adjust the Python command as needed
 python main_new_fixbugv3.py \
   --api_key 7132bb0a-223c-4fd0-9d9e-893d1bcb17b0 \
-  --input_file playground/data/domain-incremental/llava_v1_5_mix665k-ocr_vqa.json \
+  --input_file /ppio_net0/code/LLaVA/playground/data/domain-incremental/llava_v1_5_mix665k-ocr_vqa.json \
   --output_file output-qwen-v3/ocr.json \
   --base_url https://api.ppinfra.com/v3/openai \
   --model qwen/qwen2.5-32b-instruct \
