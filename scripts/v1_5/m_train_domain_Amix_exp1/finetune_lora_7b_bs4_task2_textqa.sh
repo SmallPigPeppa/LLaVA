8da0820fd9f6f290840335527f26a9cb87b5d6b3
lwz <905328134@qq.com>
2024-12-24 07:27:21 +0800

    initial

--- scripts/v1_5/m_train_domain_Amix_exp1/finetune_lora_7b_bs4_task2_textqa.sh
+++ scripts/v1_5/m_train_domain_Amix_exp1/finetune_lora_7b_bs4_task2_textqa.sh
@@ -7,7 +7,7 @@ export HF_HOME=/ppio_net0/huggingface
 # Manually specify model and vision configuration
 MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"  # Update this path with your specific model path
 VISION_TOWER="openai/clip-vit-large-patch14-336"
-DATA_PATH="playground/data/domain-incremental/llava_v1_5_mix665k-gqa.json"
+DATA_PATH="playground/data/domain-incremental/llava_v1_5_mix665k-textvqa.json"
 OUTPUT_DIR="continual-ckpt/exp1/llava-v1.5-7b-lora-task-gqa"
 
 # Training command for OCR task
