0beee880ddf516bd40e4a19a2a00b2e32c449a20
lwz <905328134@qq.com>
2024-12-24 01:23:56 +0800

    initial

---
+++ A-mix/promptv4_plus.py
@@ -0,0 +1,9 @@
+rule_description = """
+## Objective:
+Improve the quality of answers (A) through the following ways:
+
+1. The content with "from": "human" in the JSON file corresponds to Q (question), and the content with "from": "gpt" corresponds to A (answer).
+2. Iterate through each QA pair in the JSON file and rewrite A (the answer) in a more flexible and varied way.
+
+"""
+
