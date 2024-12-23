96e6dc6441f4b9e8f4df79b320661be358ab9642
lwz <905328134@qq.com>
2024-12-23 23:51:04 +0800

    initial

--- A-mix/run_Amix_ocr_exp1_v5.sh
+++ A-mix/run_Amix_ocr_exp1_v5.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python main_new_fixbugv4.py \
+python main_new_fixbugv5.py \
   --api_key 7132bb0a-223c-4fd0-9d9e-893d1bcb17b0 \
   --input_file /ppio_net0/code/LLaVA/playground/data/exp1/part2-Aold.json \
   --output_file /ppio_net0/code/LLaVA/playground/data/exp1/part2-mix-v5.json \
