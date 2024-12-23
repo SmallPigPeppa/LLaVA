60d567d8d9df2b541482a57598cbf84c605af9dd
lwz <905328134@qq.com>
2024-12-24 05:22:45 +0800

    initial

--- A-mix/promptv7.py
+++ A-mix/promptv7.py
@@ -5,7 +5,7 @@ Improve the quality of questions ("from": "human") and answers ("from": "gpt") t
 
 Step 1: Rewrite every A into a smooth and natural paragraph. Remove the constrain "\nAnswer the question using a single word or phrase. in Q1"
 Step 2: randomly generate 2~4 new questions ("from": "human") and answers ("from": "gpt") pairs related to the image (answers should be between 50–100 words) and add them to the "conversations."
-Step 3:
+Step 3: Return the updated JSON data, following the format of the JSON file.
 
 """
 
