149de8751170cec8e60e765f8d53be6a29a7fd0c
lwz <905328134@qq.com>
2024-12-24 00:54:37 +0800

    initial

--- scripts/v1_5/m_train_domain_Amix_exp1/merge.sh
+++ scripts/v1_5/m_train_domain_Amix_exp1/merge.sh
@@ -3,8 +3,8 @@
 # Set Hugging Face cache directory
 export HF_HOME=/ppio_net0/huggingface
 MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
-MODEL_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-ocr-mix"
-OUT_PATH=${MODEL_PATH}-merged-exp1-mix
+MODEL_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-ocr-mix-v5"
+OUT_PATH=${MODEL_PATH}-merged
 
 python -m llava.eval.model_vqa_save_weight_hf \
   --model-path ${MODEL_PATH} \
