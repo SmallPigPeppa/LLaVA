50b2da301927f5e4201d8780d9a76258ca9ebd5b
lwz <905328134@qq.com>
2024-12-24 07:56:47 +0800

    initial

--- scripts/v1_5/m_train_domain_Amix_exp1/llavabench.sh
+++ scripts/v1_5/m_train_domain_Amix_exp1/llavabench.sh
@@ -5,8 +5,8 @@ export CUDA_VISIBLE_DEVICES=0
 MODEL="llava-v1.5-7b-lora-task-ocr-mix-v7-merged"
 
 export HF_HOME=/ppio_net0/huggingface
-MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
-MODEL_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-ocr-mix-v7"
+MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged-exp1"
+MODEL_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-gqa"
 OUT_PATH=${MODEL_PATH}-merged
 
 python -m llava.eval.model_vqa_save_weight_hf \
