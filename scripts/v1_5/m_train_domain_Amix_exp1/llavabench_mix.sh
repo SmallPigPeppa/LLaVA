8e0f6e4ef4486b7eb1bd963d687d85d18391b47f
lwz <905328134@qq.com>
2024-12-24 10:21:25 +0800

    initial

--- scripts/v1_5/m_train_domain_Amix_exp1/llavabench_mix.sh
+++ scripts/v1_5/m_train_domain_Amix_exp1/llavabench_mix.sh
@@ -2,28 +2,28 @@
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 # Set model as a variable
-MODEL="llava-v1.5-7b-lora-task-gqa-merged-exp1-ratio0"
+MODEL="llava-v1.5-7b-lora-task-gqa-merged-exp1-ratio0.01"
 
 export HF_HOME=/ppio_net0/huggingface
 MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 MODEL_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-gqa"
-OUT_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-gqa-merged-exp1-ratio0"
+OUT_PATH="continual-ckpt/exp1/llava-v1.5-7b-lora-task-gqa-merged-exp1-ratio0.01"
 
-python -m llava.eval.model_vqa_save_weight_hf_mix \
-  --model-path ${MODEL_PATH} \
-  --model-base ${MODEL_BASE} \
-  --save-path ${OUT_PATH} \
-  --lora-scale-factor 0
-
-
-# Evaluate the model
-python -m llava.eval.model_vqa \
-    --model-path "continual-ckpt/exp1/$MODEL" \
-    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
-    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
-    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
-    --temperature 0 \
-    --conv-mode vicuna_v1
+#python -m llava.eval.model_vqa_save_weight_hf_mix \
+#  --model-path ${MODEL_PATH} \
+#  --model-base ${MODEL_BASE} \
+#  --save-path ${OUT_PATH} \
+#  --lora-scale-factor 0
+#
+#
+## Evaluate the model
+#python -m llava.eval.model_vqa \
+#    --model-path "continual-ckpt/exp1/$MODEL" \
+#    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+#    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
+#    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+#    --temperature 0 \
+#    --conv-mode vicuna_v1
 
 # Create the reviews directory
 mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
