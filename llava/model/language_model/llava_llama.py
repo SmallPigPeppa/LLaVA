040dac5a518c7c76b0f26a217d9bd67ef1bb8a78
lwz <905328134@qq.com>
2024-12-25 02:29:59 +0800

    initial

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -54,7 +54,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
     def __init__(self, config):
         super(LlamaForCausalLM, self).__init__(config)
         self.model = LlavaLlamaModel(config)
-        self.model_old = None
+        # self.model_old = None
         self.pretraining_tp = config.pretraining_tp
         self.vocab_size = config.vocab_size
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
