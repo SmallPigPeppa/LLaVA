d27e5b324b527f92c75b4b706403c0e209f183f2
lwz <905328134@qq.com>
2024-12-24 14:27:13 +0800

    initial

--- scripts/v1_5/m_train_domain_Amix_exp1/textvqa.sh
+++ scripts/v1_5/m_train_domain_Amix_exp1/textvqa.sh
@@ -2,9 +2,24 @@
 
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=1
-MODEL="llava-v1.5-7b-lora-task-gqa-merged-exp1-ratio0.05"
+#MODEL="llava-v1.5-7b-lora-task-gqa-merged-exp1-ratio0.05"
+#python -m llava.eval.model_vqa_loader \
+#    --model-path "continual-ckpt/exp1/$MODEL" \
+#    --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
+#    --image-folder ./playground/data/eval/textvqa/train_images \
+#    --answers-file ./playground/data/eval/textvqa/answers/$MODEL.jsonl \
+#    --temperature 0 \
+#    --conv-mode vicuna_v1
+#
+#python -m llava.eval.eval_textvqa \
+#    --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
+#    --result-file ./playground/data/eval/textvqa/answers/$MODEL.jsonl
+
+
+
+MODEL="llava-v1.5-7b-lora-task-coco-merged"
 python -m llava.eval.model_vqa_loader \
-    --model-path "continual-ckpt/exp1/$MODEL" \
+    --model-path "continual-ckpt/domain/$MODEL" \
     --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
     --image-folder ./playground/data/eval/textvqa/train_images \
     --answers-file ./playground/data/eval/textvqa/answers/$MODEL.jsonl \
