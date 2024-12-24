ab4ade1c47b74f3bcb9c425cad31958b27e490d6
lwz <905328134@qq.com>
2024-12-24 23:28:18 +0800

    initial

--- llava/model/language_model/llava_llama_origin.py
+++ llava/model/language_model/llava_llama_origin.py
@@ -18,18 +18,8 @@ from typing import List, Optional, Tuple, Union
 import torch
 import torch.nn as nn
 
-# from transformers import AutoConfig, AutoModelForCausalLM, \
-#     LlamaConfig, LlamaModel, LlamaForCausalLM
-
-################
-# Modified
 from transformers import AutoConfig, AutoModelForCausalLM, \
-                         LlamaConfig, LlamaModel
-from .my_llama import LlamaForCausalLM
-from llava.constants import IMAGE_TOKEN_INDEX
-
-################
-
+    LlamaConfig, LlamaModel, LlamaForCausalLM
 
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from transformers.generation.utils import GenerateOutput
@@ -80,11 +70,6 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
 
-
-        multi_modal_index = [i for i in range(input_ids.shape[0]) if IMAGE_TOKEN_INDEX in input_ids[i]]
-        pure_text_index = [i for i in range(input_ids.shape[0]) if IMAGE_TOKEN_INDEX not in input_ids[i]]
-        import pdb; pdb.set_trace()
-
         if inputs_embeds is None:
             (
                 input_ids,
@@ -102,7 +87,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 images,
                 image_sizes
             )
-        # import pdb;pdb.set_trace()
+
         out_dict = super().forward(
             input_ids=input_ids,
             attention_mask=attention_mask,
