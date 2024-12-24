48bf482ab79253f5b926dd33222cefd9ee49306d
lwz <905328134@qq.com>
2024-12-24 21:25:02 +0800

    initial

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -27,8 +27,8 @@ from transformers import AutoConfig, AutoModelForCausalLM, \
 # from transformers import AutoConfig, AutoModelForCausalLM, \
 #                          LlamaConfig, LlamaModel
 # from .my_llama import LlamaForCausalLM
-# from transformers.models.llama.modeling_llama import add_start_docstrings_to_model_forward, \
-#     LLAMA_INPUTS_DOCSTRING, replace_return_docstrings, Cache, CrossEntropyLoss, F
+from transformers.models.llama.modeling_llama import add_start_docstrings_to_model_forward, \
+    LLAMA_INPUTS_DOCSTRING, replace_return_docstrings, Cache, CrossEntropyLoss, F
 # _CONFIG_FOR_DOC = "LlamaConfig"
 
 ################
@@ -70,75 +70,75 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
 
     # @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
     # @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
-    # def llama_forward(
-    #     self,
-    #     input_ids: torch.LongTensor = None,
-    #     attention_mask: Optional[torch.Tensor] = None,
-    #     position_ids: Optional[torch.LongTensor] = None,
-    #     past_key_values: Optional[Union[Cache, List[torch.FloatTensor]]] = None,
-    #     inputs_embeds: Optional[torch.FloatTensor] = None,
-    #     labels: Optional[torch.LongTensor] = None,
-    #     use_cache: Optional[bool] = None,
-    #     output_attentions: Optional[bool] = None,
-    #     output_hidden_states: Optional[bool] = None,
-    #     return_dict: Optional[bool] = None,
-    #     cache_position: Optional[torch.LongTensor] = None,
-    # ) -> Union[Tuple, CausalLMOutputWithPast]:
-    #     output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-    #     output_hidden_states = (
-    #         output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-    #     )
-    #     return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-    #
-    #     # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
-    #     outputs = self.model(
-    #         input_ids=input_ids,
-    #         attention_mask=attention_mask,
-    #         position_ids=position_ids,
-    #         past_key_values=past_key_values,
-    #         inputs_embeds=inputs_embeds,
-    #         use_cache=use_cache,
-    #         output_attentions=output_attentions,
-    #         output_hidden_states=output_hidden_states,
-    #         return_dict=return_dict,
-    #         cache_position=cache_position,
-    #     )
-    #
-    #     hidden_states = outputs[0]
-    #     if self.config.pretraining_tp > 1:
-    #         lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
-    #         logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
-    #         logits = torch.cat(logits, dim=-1)
-    #     else:
-    #         logits = self.lm_head(hidden_states)
-    #     logits = logits.float()
-    #
-    #     import pdb; pdb.set_trace()
-    #
-    #     loss = None
-    #     if labels is not None:
-    #         # Shift so that tokens < n predict n
-    #         shift_logits = logits[..., :-1, :].contiguous()
-    #         shift_labels = labels[..., 1:].contiguous()
-    #         # Flatten the tokens
-    #         loss_fct = CrossEntropyLoss()
-    #         shift_logits = shift_logits.view(-1, self.config.vocab_size)
-    #         shift_labels = shift_labels.view(-1)
-    #         # Enable model parallelism
-    #         shift_labels = shift_labels.to(shift_logits.device)
-    #         loss = loss_fct(shift_logits, shift_labels)
-    #
-    #     if not return_dict:
-    #         output = (logits,) + outputs[1:]
-    #         return (loss,) + output if loss is not None else output
-    #
-    #     return CausalLMOutputWithPast(
-    #         loss=loss,
-    #         logits=logits,
-    #         past_key_values=outputs.past_key_values,
-    #         hidden_states=outputs.hidden_states,
-    #         attentions=outputs.attentions,
-    #     )
+    def llama_forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[Union[Cache, List[torch.FloatTensor]]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            cache_position=cache_position,
+        )
+
+        hidden_states = outputs[0]
+        if self.config.pretraining_tp > 1:
+            lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+            logits = torch.cat(logits, dim=-1)
+        else:
+            logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        import pdb; pdb.set_trace()
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
     def forward(
         self,
