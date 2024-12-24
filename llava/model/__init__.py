c35945f05c79a24c051e1647248597f7f16717c7
lwz <905328134@qq.com>
2024-12-24 20:43:30 +0800

    initial

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -1,4 +1,5 @@
 try:
+    from .language_model.my_llama import LlamaForCausalLM
     from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
     from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
     from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
