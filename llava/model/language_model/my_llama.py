eb14b466699830f940a67e1e31f54a124defa818
lwz <905328134@qq.com>
2024-12-25 02:33:15 +0800

    initial

--- llava/model/language_model/my_llama.py
+++ llava/model/language_model/my_llama.py
@@ -203,6 +203,30 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict,
             )
+            output1 = self.model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_values=past_key_values,
+                inputs_embeds=inputs_embeds,
+                use_cache=use_cache,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+            output2 = self.base_model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_values=past_key_values,
+                inputs_embeds=inputs_embeds,
+                use_cache=use_cache,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+            import pdb;pdb.set_trace()
+
 
             hidden_states_old = outputs_old[0]
 
