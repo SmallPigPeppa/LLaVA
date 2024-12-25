e7944c5517f59cc29ad2268b5b5339d8a7ebc00e
lwz <905328134@qq.com>
2024-12-26 01:59:40 +0800

    initial

--- llava/model/language_model/my_llama_debug.py
+++ llava/model/language_model/my_llama_debug.py
@@ -28,9 +28,9 @@ import torch.utils.checkpoint
 from torch import nn
 from torch.nn import CrossEntropyLoss
 from transformers.models.llama.modeling_llama import Cache
-# from transformers.models.llama.modeling_llama import (
-#     CausalLMOutputWithPast,
-# )
+from transformers.models.llama.modeling_llama import (
+    CausalLMOutputWithPast,
+)
 
 from transformers.models.llama.modeling_llama import (
     is_flash_attn_2_available,
@@ -70,44 +70,44 @@ class ForwardKLLoss(torch.nn.Module):
         # Loss is averaged over non-ignored targets
         return -torch.sum(x * mask.view(-1), dim=0) / torch.sum(mask.view(-1), dim=0)
 
-from transformers.modeling_outputs import dataclass,ModelOutput
-
-@dataclass
-class CausalLMOutputWithPast(ModelOutput):
-    """
-    Base class for causal language model (or autoregressive) outputs.
-
-    Args:
-        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
-            Language modeling loss (for next-token prediction).
-        logits (`torch.FloatTensor` of shape `(batch_size, sequence_length, config.vocab_size)`):
-            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
-        past_key_values (`tuple(tuple(torch.FloatTensor))`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
-            Tuple of `tuple(torch.FloatTensor)` of length `config.n_layers`, with each tuple having 2 tensors of shape
-            `(batch_size, num_heads, sequence_length, embed_size_per_head)`)
-
-            Contains pre-computed hidden-states (key and values in the self-attention blocks) that can be used (see
-            `past_key_values` input) to speed up sequential decoding.
-        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
-            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
-            one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
-
-            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
-        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
-            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
-            sequence_length)`.
-
-            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
-            heads.
-    """
-
-    loss: Optional[torch.FloatTensor] = None
-    kd_loss: Optional[torch.FloatTensor] = None
-    llava_loss: Optional[torch.FloatTensor] = None
-    logits: torch.FloatTensor = None
-    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
-    hidden_states: Optional[Tuple[torch.FloatTensor, ...]] = None
-    attentions: Optional[Tuple[torch.FloatTensor, ...]] = None
+# from transformers.modeling_outputs import dataclass,ModelOutput
+#
+# @dataclass
+# class CausalLMOutputWithPast(ModelOutput):
+#     """
+#     Base class for causal language model (or autoregressive) outputs.
+#
+#     Args:
+#         loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+#             Language modeling loss (for next-token prediction).
+#         logits (`torch.FloatTensor` of shape `(batch_size, sequence_length, config.vocab_size)`):
+#             Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+#         past_key_values (`tuple(tuple(torch.FloatTensor))`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
+#             Tuple of `tuple(torch.FloatTensor)` of length `config.n_layers`, with each tuple having 2 tensors of shape
+#             `(batch_size, num_heads, sequence_length, embed_size_per_head)`)
+#
+#             Contains pre-computed hidden-states (key and values in the self-attention blocks) that can be used (see
+#             `past_key_values` input) to speed up sequential decoding.
+#         hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+#             Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+#             one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
+#
+#             Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+#         attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+#             Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+#             sequence_length)`.
+#
+#             Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+#             heads.
+#     """
+#
+#     loss: Optional[torch.FloatTensor] = None
+#     kd_loss: Optional[torch.FloatTensor] = None
+#     llava_loss: Optional[torch.FloatTensor] = None
+#     logits: torch.FloatTensor = None
+#     past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
+#     hidden_states: Optional[Tuple[torch.FloatTensor, ...]] = None
+#     attentions: Optional[Tuple[torch.FloatTensor, ...]] = None
 
 class LlamaForCausalLM(LlamaPreTrainedModel):
     _tied_weights_keys = ["lm_head.weight"]
@@ -317,9 +317,7 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
             logits=logits,
             past_key_values=outputs.past_key_values,
             hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-            kd_loss=kd_loss,
-            llava_loss=llava_loss
+            attentions=outputs.attentions
         )
 
     def prepare_inputs_for_generation(
