e74339872c5c24b8c36916c7dd223d16a61c5545
lwz <905328134@qq.com>
2024-12-25 11:56:13 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/AL20_llavabench.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+export HF_HOME=/mnt/disk3/wzliu/huggingface
+export CUDA_VISIBLE_DEVICES=0
+# Set model as a variable
+MODEL="llava-v1.5-7b-lora-task-ocr-distill-merged"
+
+export HF_HOME=/ppio_net0/huggingface
+MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
+MODEL_PATH="continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill"
+OUT_PATH=${MODEL_PATH}-merged
+
+python -m llava.eval.model_vqa_save_weight_hf \
+  --model-path ${MODEL_PATH} \
+  --model-base ${MODEL_BASE} \
+  --save-path ${OUT_PATH}
+
+
+# Evaluate the model
+python -m llava.eval.model_vqa \
+    --model-path "continual-ckpt/distill/$MODEL" \
+    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
+    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+# Create the reviews directory
+mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
+
+# Generate reviews
+python llava/eval/eval_gpt_review_bench.py \
+    --question playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --context playground/data/eval/llava-bench-in-the-wild/context.jsonl \
+    --rule llava/eval/table/rule.json \
+    --answer-list \
+        playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
+        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+    --output \
+        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL.jsonl
+
+# Summarize reviews
+python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL.jsonl
+
+
