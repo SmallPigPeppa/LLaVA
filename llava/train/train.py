9f4284dfa4453a2e24161b6a7867f3d1f98134a5
lwz <905328134@qq.com>
2024-12-26 03:41:39 +0800

    initial

--- llava/train/train.py
+++ llava/train/train.py
@@ -1066,11 +1066,13 @@ def train(attn_implementation=None):
                     # 获取当前的 state_dict
                     state_dict_x = model.base_model.model.model.state_dict()
 
-                    # 过滤掉包含 'vision_tower' 键的部分
-                    filtered_state_dict = {key: value for key, value in state_dict_x.items() if 'vision_tower' not in key}
-
-                    # 加载过滤后的 state_dict
-                    model.model_old.load_state_dict(filtered_state_dict)
+                    # # 过滤掉包含 'vision_tower' 键的部分
+                    # filtered_state_dict = {key: value for key, value in state_dict_x.items() if 'vision_tower' not in key}
+                    #
+                    # # 加载过滤后的 state_dict
+                    # model.model_old.load_state_dict(filtered_state_dict)
+                    import pdb;pdb.set_trace()
+                    model.base_model.model.model_old.load_state_dict(state_dict_x)
 
                     print("成功将 model 的参数复制到 model_old。")
                 except Exception as e:
