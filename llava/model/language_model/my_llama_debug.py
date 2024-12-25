56fcf61f78cafdbb7b4da6eb5610437474d4fc61
lwz <905328134@qq.com>
2024-12-26 06:22:17 +0800

    initial

--- llava/model/language_model/my_llama_debug.py
+++ llava/model/language_model/my_llama_debug.py
@@ -217,23 +217,22 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
             shift_labels = shift_labels.to(shift_logits.device)  # 确保标签在相同的设备上
             llava_loss = loss_fct(shift_logits, shift_labels)
 
-        # kd_loss = llava_loss
-        # kd_loss_ce = llava_loss
+
         # 蒸馏损失计算
-        # if len(pure_text_index) > 0:
-        #     # 获取旧模型输出
-        #     with torch.no_grad():
-        #         outputs_old = self.model_old(
-        #             input_ids=input_ids,
-        #             attention_mask=attention_mask,
-        #             position_ids=position_ids,
-        #             past_key_values=past_key_values,
-        #             inputs_embeds=inputs_embeds,
-        #             use_cache=use_cache,
-        #             output_attentions=output_attentions,
-        #             output_hidden_states=output_hidden_states,
-        #             return_dict=return_dict,
-        #         )
+        if len(pure_text_index) > 0:
+            # 获取旧模型输出
+            with torch.no_grad():
+                outputs_old = self.model_old(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_values=past_key_values,
+                    inputs_embeds=inputs_embeds,
+                    use_cache=use_cache,
+                    output_attentions=output_attentions,
+                    output_hidden_states=output_hidden_states,
+                    return_dict=return_dict,
+                )
         #     hidden_states_old = outputs_old[0]
         #
         #     # 计算旧模型的 logits
