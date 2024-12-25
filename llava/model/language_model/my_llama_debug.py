9a5e4a40b6de9c7fd0b6423086ecc5a7b72b5966
lwz <905328134@qq.com>
2024-12-26 06:34:28 +0800

    initial

--- llava/model/language_model/my_llama_debug.py
+++ llava/model/language_model/my_llama_debug.py
@@ -229,7 +229,7 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
                 position_ids=position_ids,
                 past_key_values=past_key_values,
                 inputs_embeds=inputs_embeds,
-                use_cache=False,
+                use_cache=use_cache,
                 output_attentions=output_attentions,
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict,
