d6d866e8de1100f40f32701d8421f0363f3620b0
lwz <905328134@qq.com>
2024-12-26 05:23:56 +0800

    initial

--- llava/model/language_model/my_llama_debug.py
+++ llava/model/language_model/my_llama_debug.py
@@ -249,13 +249,15 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
             # hidden_states_old = outputs[0]
 
             # 计算旧模型的 logits
-            if self.config.pretraining_tp > 1:
-                lm_head_slices_old = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
-                logits_old = [F.linear(hidden_states_old, lm_head_slices_old[i]) for i in
-                              range(self.config.pretraining_tp)]
-                logits_old = torch.cat(logits_old, dim=-1)
-            else:
-                logits_old = self.lm_head(hidden_states_old)
+            with torch.no_grad():
+                if self.config.pretraining_tp > 1:
+                    lm_head_slices_old = self.lm_head_old.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+                    logits_old = [F.linear(hidden_states_old, lm_head_slices_old[i]) for i in
+                                  range(self.config.pretraining_tp)]
+                    logits_old = torch.cat(logits_old, dim=-1)
+                else:
+                    print("here")
+                    logits_old = self.lm_head_old(hidden_states_old)
 
             logits_pure_text = logits[pure_text_index].clone()
             logits_pure_text_old = logits_old[pure_text_index].clone()
