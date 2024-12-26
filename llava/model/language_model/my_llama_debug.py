259da6034c0a8cf0917b223c59aae2ce0ae18e70
lwz <905328134@qq.com>
2024-12-26 22:08:54 +0800

    initial

--- llava/model/language_model/my_llama_debug.py
+++ llava/model/language_model/my_llama_debug.py
@@ -250,13 +250,17 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
         #         shift_labels_text
         #     )
         kd_loss = loss_x * 0.
+
+
         # import pdb;pdb.set_trace()
         if kd_loss is not None and llava_loss is not None:
             loss = kd_loss * 0. + llava_loss
-            self.report_metrics(kd_loss=kd_loss, kd_loss_ce=kd_loss_ce, llava_loss=llava_loss, all_loss=loss)
+            # self.report_metrics(kd_loss=kd_loss, kd_loss_ce=kd_loss_ce, llava_loss=llava_loss, all_loss=loss)
+            self.report_metrics(kd_loss=kd_loss, llava_loss=llava_loss, all_loss=loss)
         elif kd_loss is not None:
             loss = kd_loss * 0.
-            self.report_metrics(kd_loss=kd_loss, kd_loss_ce=kd_loss_ce, all_loss=loss)
+            # self.report_metrics(kd_loss=kd_loss, kd_loss_ce=kd_loss_ce, all_loss=loss)
+            self.report_metrics(kd_loss=kd_loss,  all_loss=loss)
         elif llava_loss is not None:
             loss = llava_loss
             self.report_metrics(llava_loss=llava_loss, all_loss=loss)
