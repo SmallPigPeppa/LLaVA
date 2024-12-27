35c863f6c90ab59769bed3f542e63c5431dde9fb
lwz <905328134@qq.com>
2024-12-27 15:19:10 +0800

    initial

--- scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train_multi_gpu.sh
+++ scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train_multi_gpu.sh
@@ -2,7 +2,8 @@
 
 # 设置Hugging Face缓存目录
 export HF_HOME=/mnt/disk3/wzliu/huggingface
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3  # 限定GPU为0, 1, 2, 3
+
 # 定义模型和任务相关参数
 MODEL="llava-v1.5-7b-wo-train"
 
@@ -14,9 +15,9 @@ OUTPUT_FILE="./playground/data/domain-incremental/llava_v1_5_mix665k-others-Aold
 TMP_DIR="./c-llava-cache-${OUTPUT_FILE##*/}"
 mkdir -p $TMP_DIR
 
-# 检测可用的GPU数量
-NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
-echo "检测到 $NUM_GPUS 个GPU。"
+# 检测可用的GPU数量，只考虑0,1,2,3
+NUM_GPUS=4  # 固定为4个GPU
+echo "检测到 $NUM_GPUS 个GPU（限定为0,1,2,3）。"
 
 # 使用jq拆分数据集为NUM_GPUS部分
 echo "使用jq将数据集拆分为 $NUM_GPUS 部分..."
