c34a83fbb315e5f6e65904bc444398af3e4ee9cf
lwz <905328134@qq.com>
2024-12-27 13:46:07 +0800

    initial

--- scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train.sh
+++ scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train.sh
@@ -2,7 +2,7 @@
 export HF_HOME=/mnt/disk3/wzliu/huggingface
 
 # Set the model for the current task
-MODEL="llava-v1.5-7b-lora-wo-train-merged"
+MODEL="llava-v1.5-7b-wo-train"
 
 # Evaluate the model
 echo "Generate A-old from $MODEL"
@@ -10,7 +10,7 @@ echo "Generate A-old from $MODEL"
 python -m llava.eval.model_vqa_generate_Aold \
     --model-path "continual-ckpt/distill/$MODEL" \
     --image-folder ./playground/data \
-    --dataset-file ./playground/data/debug/llava_v1_5_mix665k-random-8.json \
-    --output-file ./playground/data/debug/llava_v1_5_mix665k-random-8-Aold.json \
+    --dataset-file ./playground/data/domain-incremental/others.json \
+    --output-file /playground/data/domain-incremental/others-Aold.json \
     --conv-mode llava_v1
 
