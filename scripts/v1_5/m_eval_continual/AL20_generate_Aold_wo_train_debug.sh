afad70287a9642aec5104a3f3c5ad8f538d336e0
lwz <905328134@qq.com>
2024-12-28 01:34:20 +0800

    initial

---
+++ scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train_debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export HF_HOME=/mnt/disk3/wzliu/huggingface
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# Set the model for the current task
+MODEL="llava-v1.5-7b-wo-train"
+
+# Evaluate the model
+echo "Generate A-old from $MODEL"
+
+python -m llava.eval.model_vqa_generate_Aold_text \
+    --model-path "continual-ckpt/distill/$MODEL" \
+    --image-folder ./playground/data \
+    --dataset-file playground/data/exp1/part2-others-debug.json \
+    --output-file .playground/data/exp1/part2-others-debug-Aold.json \
+    --conv-mode llava_v1
+
