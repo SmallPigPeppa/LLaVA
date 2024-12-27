8eebe5a830d6172a8a025d2a070327d78c89bca0
lwz <905328134@qq.com>
2024-12-27 14:59:43 +0800

    initial

--- scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train.sh
+++ scripts/v1_5/m_eval_continual/AL20_generate_Aold_wo_train.sh
@@ -7,7 +7,7 @@ MODEL="llava-v1.5-7b-wo-train"
 # Evaluate the model
 echo "Generate A-old from $MODEL"
 
-python -m llava.eval.model_vqa_generate_Aold \
+python -m llava.eval.model_vqa_generate_Aold_text \
     --model-path "continual-ckpt/distill/$MODEL" \
     --image-folder ./playground/data \
     --dataset-file ./playground/data/domain-incremental/llava_v1_5_mix665k-others.json \
