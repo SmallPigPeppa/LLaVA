5977372444e43cfa678849ff61cd8642ae338965
lwz <905328134@qq.com>
2024-12-28 19:51:20 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_llavabench.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_llavabench.sh
@@ -8,6 +8,7 @@ export CUDA_VISIBLE_DEVICES=0
 MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 MODEL_PATH="continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill-mix"
 MODEL_PATH="continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill-lambda1.0-datamix-mix"
+MODEL_PATH="continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-no-distill-datamix-mix"
 OUT_PATH="${MODEL_PATH}-merged"
 MODEL="${OUT_PATH##*/}"
 
