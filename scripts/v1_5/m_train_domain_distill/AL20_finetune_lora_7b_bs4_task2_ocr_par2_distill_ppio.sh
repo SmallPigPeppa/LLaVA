1c71374bfc406b4cb51d4115f5467c8ad8557e92
lwz <905328134@qq.com>
2024-12-29 03:27:30 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_finetune_lora_7b_bs4_task2_ocr_par2_distill_ppio.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_finetune_lora_7b_bs4_task2_ocr_par2_distill_ppio.sh
@@ -2,6 +2,7 @@
 
 # Set Hugging Face cache directory
 export HF_HOME=/ppio_net0/huggingface
+export HF_HOME=/mnt/disk3/wzliu/huggingface
 # Manually specify model and vision configuration
 MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 VISION_TOWER="openai/clip-vit-large-patch14-336"
