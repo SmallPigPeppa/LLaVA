2f1d59a6cb4144f01684af4b276a51b8a193091a
lwz <905328134@qq.com>
2025-01-05 18:46:34 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_finetune_lora_7b_bs4_task2_ocr_par2_distill_mse_random.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_finetune_lora_7b_bs4_task2_ocr_par2_distill_mse_random.sh
@@ -30,9 +30,9 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir ${OUTPUT_DIR} \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
