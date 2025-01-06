58c215843c2179c16604d8c8f2b74446bcc5cfa8
lwz <905328134@qq.com>
2025-01-06 21:45:15 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme_domain.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme_domain.sh
@@ -13,15 +13,15 @@ python -m llava.eval.model_vqa_loader \
     --model-path $MODEL \
     --question-file ./playground/data/eval/MME/llava_mme.jsonl \
     --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
-    --answers-file ./playground/data/eval/MME/answers/llava-v1.5-13b.jsonl \
+    --answers-file ./playground/data/eval/MME/answers/llava-v1.5-7b-lora-task-others-merged.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 cd ./playground/data/eval/MME
 
-python convert_answer_to_mme.py --experiment llava-v1.5-13b
+python convert_answer_to_mme.py --experiment llava-v1.5-7b-lora-task-others-merged
 
 cd eval_tool
 
-python calculation.py --results_dir answers/llava-v1.5-13b
+python calculation.py --results_dir answers/llava-v1.5-7b-lora-task-others-merged
 
