34235f26cec9cd4063d7742926a09cfd67573634
lwz <905328134@qq.com>
2025-01-06 18:46:12 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mmbench_domain.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mmbench_domain.sh
@@ -3,22 +3,36 @@ export HF_HOME=/mnt/disk3/wzliu/huggingface
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 SPLIT="mmbench_dev_20230712"
-MODEL=continual-ckpt/domain-Amix/llava-v1.5-7b-lora-task-ocr-merged
-MODEL=continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill-lambda100.0-nodatamix-mse-all-mix-merged
-MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged
-MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged
-python -m llava.eval.model_vqa_mmbench \
-    --model-path $MODEL \
-    --question-file ./playground/data/eval/mmbench/$SPLIT.tsv \
-    --answers-file ./playground/data/eval/mmbench/answers/$SPLIT/llava-v1.5-7b-lora-task-gqa-merged.jsonl \
-    --single-pred-prompt \
-    --temperature 0 \
-    --conv-mode vicuna_v1
 
-mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+# List of model paths
+MODELS=(
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
+#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged"
+#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged"
+)
 
-python scripts/convert_mmbench_for_submission.py \
-    --annotation-file ./playground/data/eval/mmbench/$SPLIT.tsv \
-    --result-dir ./playground/data/eval/mmbench/answers/$SPLIT \
-    --upload-dir ./playground/data/eval/mmbench/answers_upload/$SPLIT \
-    --experiment llava-v1.5-7b-lora-task-gqa-merged
+# Iterate over models and run evaluation
+for MODEL in "${MODELS[@]}"; do
+    echo "Evaluating model: $MODEL"
+
+    # Run evaluation for the current model
+    python -m llava.eval.model_vqa_mmbench \
+        --model-path $MODEL \
+        --question-file ./playground/data/eval/mmbench/$SPLIT.tsv \
+        --answers-file ./playground/data/eval/mmbench/answers/$SPLIT/$(basename $MODEL).jsonl \
+        --single-pred-prompt \
+        --temperature 0 \
+        --conv-mode vicuna_v1
+
+    # Create directory for results
+    mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+
+    # Convert results for submission
+    python scripts/convert_mmbench_for_submission.py \
+        --annotation-file ./playground/data/eval/mmbench/$SPLIT.tsv \
+        --result-dir ./playground/data/eval/mmbench/answers/$SPLIT \
+        --upload-dir ./playground/data/eval/mmbench/answers_upload/$SPLIT \
+        --experiment $(basename $MODEL)
+done
