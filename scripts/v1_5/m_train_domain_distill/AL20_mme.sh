f411fd5adf473a9d3beb640396c4f56e12f7239f
lwz <905328134@qq.com>
2025-01-06 22:53:34 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme.sh
@@ -2,9 +2,10 @@
 export HF_HOME=/mnt/disk3/wzliu/huggingface
 #export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=7
-
+MODEL=checkpoints/llava-v1.5-7b-lora-merged
+MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged
 python -m llava.eval.model_vqa_loader \
-    --model-path checkpoints/llava-v1.5-7b-lora-merged \
+    --model-path $MODEL \
     --question-file ./playground/data/eval/MME/llava_mme.jsonl \
     --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
     --answers-file ./playground/data/eval/MME/answers/llava-v1.5-13b.jsonl \
