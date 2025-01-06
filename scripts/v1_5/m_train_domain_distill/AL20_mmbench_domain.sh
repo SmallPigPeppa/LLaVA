75ec7fea381d9391c3ef4de12e4e4920682d9418
lwz <905328134@qq.com>
2025-01-06 21:35:48 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mmbench_domain.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mmbench_domain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export HF_HOME=/mnt/disk3/wzliu/huggingface
 export HF_HOME=/ppio_net0/huggingface
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=6
 SPLIT="mmbench_dev_20230712"
 
 # List of model paths
