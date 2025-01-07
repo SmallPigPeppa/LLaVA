468edbc08b7e4980cc0a2bd6fd3346a3bfcbe356
lwz <905328134@qq.com>
2025-01-07 20:21:45 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_task1_coco_mse_ppiov2.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_task1_coco_mse_ppiov2.sh
@@ -2,8 +2,8 @@
 
 # Set Hugging Face cache directory
 export HF_HOME=/ppio_net0/huggingface
-export HF_HOME=/mnt/disk3/wzliu/huggingface
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+#export HF_HOME=/mnt/disk3/wzliu/huggingface
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
 # Manually specify model and vision configuration
 MODEL_PATH="lmsys/vicuna-7b-v1.5"
 VISION_TOWER="openai/clip-vit-large-patch14-336"
@@ -35,7 +35,7 @@ deepspeed llava/train/train_mem.py \
     --max_steps -1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
@@ -52,4 +52,4 @@ deepspeed llava/train/train_mem.py \
     --lazy_preprocess True \
     --report_to wandb
 
-#/ppio_net0/code/openapi.sh stop 1ad06ee0e8bb7b15
+/ppio_net0/code/openapi.sh stop 1ad06ee0e8bb7b15
