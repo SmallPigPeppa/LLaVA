1aa1dc5c6af1cf87a85bcfba5075013cef1ac7e8
lwz <905328134@qq.com>
2025-01-11 15:05:02 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_abexp_coco_text_4090.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_abexp_coco_text_4090.sh
@@ -12,6 +12,7 @@ VISION_TOWER="openai/clip-vit-large-patch14-336"
 DATA_PATH="playground/data/domain-incremental/llava_v1_5_mix665k-textvqa.json"
 OUTPUT_DIR="ablation-ckpt/exp1-model-mix/llava-v1.5-7b-lora-coco-textvqa"
 
+
 #MODEL_PATH="lmsys/vicuna-7b-v1.5"
 #PRETRAIN_ADAPTER="./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin"
 #    --pretrain_mm_mlp_adapter ${PRETRAIN_ADAPTER} \
