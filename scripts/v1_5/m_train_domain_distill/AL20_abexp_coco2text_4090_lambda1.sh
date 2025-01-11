722f13f98acad2837d7f96bb3875ea5eb189ace4
lwz <905328134@qq.com>
2025-01-11 19:43:20 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_abexp_coco2text_4090_lambda1.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_abexp_coco2text_4090_lambda1.sh
@@ -16,8 +16,8 @@ OUTPUT_DIR="ablation-ckpt/exp1-model-mix/llava-v1.5-7b-lora-coco2text"
 
 
 MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0-merged"
-DATA_PATH="playground/data/domain-incremental-mse/textvqa-with-others.json"
-OUTPUT_DIR="ablation-ckpt/exp1-model-mix/llava-v1.5-7b-lora-coco2text-lambda1"
+DATA_PATH="playground/data/domain-incremental-mse/textvqa-with-othersv2.json"
+OUTPUT_DIR="ablation-ckpt/exp1-model-mix/llava-v1.5-7b-lora-coco2text-lambda1-v2"
 
 
 
