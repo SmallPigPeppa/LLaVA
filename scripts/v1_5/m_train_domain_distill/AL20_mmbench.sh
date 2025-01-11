29aff61d93421638d1f3eafc9bfbf028d1051a00
lwz <905328134@qq.com>
2025-01-11 15:32:15 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mmbench.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mmbench.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export HF_HOME=/mnt/disk3/wzliu/huggingface
+export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 SPLIT="mmbench_dev_20230712"
 MODEL=continual-ckpt/domain-Amix/llava-v1.5-7b-lora-task-ocr-merged
