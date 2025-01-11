694807f3c982eef6c5b61240b32ff8890b094a80
lwz <905328134@qq.com>
2025-01-11 16:07:31 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mmbench.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mmbench.sh
@@ -6,9 +6,8 @@ SPLIT="mmbench_dev_20230712"
 MODEL=continual-ckpt/domain-Amix/llava-v1.5-7b-lora-task-ocr-merged
 MODEL=continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill-lambda100.0-nodatamix-mse-all-mix-merged
 MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged
-MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0"
+MODEL=continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0-merged
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda1.0"
-MODEL="${MODEL_PATH}-merged"
 python -m llava.eval.model_vqa_mmbench \
     --model-path $MODEL \
     --question-file ./playground/data/eval/mmbench/$SPLIT.tsv \
