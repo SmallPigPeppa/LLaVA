6cef1d4ee21914ac955cddd7e513fff306be6fe2
lwz <905328134@qq.com>
2025-01-14 01:06:12 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/mix_eval_llavabench_svd.sh
+++ scripts/v1_5/m_train_domain_distill/mix_eval_llavabench_svd.sh
@@ -13,38 +13,38 @@ SVD_RATIO=0.5
 SAVE_PATH="${MODEL_PATH_B}-mix${MIX_RATIO}-svd${SVD_RATIO}"
 MODEL_NAME=$(basename $SAVE_PATH)
 
-# Evaluate the model (1st command - saving weights)
-python -m llava.eval.model_vqa_save_weight_hf_mixv3_svd \
-  --model-path-a ${MODEL_PATH_A} \
-  --model-path-b ${MODEL_PATH_B} \
-  --mix-ratio ${MIX_RATIO} \
-  --save-path ${SAVE_PATH} \
-  --retain-ratio ${SVD_RATIO}
+## Evaluate the model (1st command - saving weights)
+#python -m llava.eval.model_vqa_save_weight_hf_mixv3_svd \
+#  --model-path-a ${MODEL_PATH_A} \
+#  --model-path-b ${MODEL_PATH_B} \
+#  --mix-ratio ${MIX_RATIO} \
+#  --save-path ${SAVE_PATH} \
+#  --retain-ratio ${SVD_RATIO}
 
-## Evaluate the model (2nd command - actual evaluation)
-#python -m llava.eval.model_vqa \
-#    --model-path ${SAVE_PATH} \
-#    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
-#    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
-#    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/${MODEL_NAME}.jsonl \
-#    --temperature 0 \
-#    --conv-mode vicuna_v1
-#
-#
-## Create the reviews directory
-#mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
-#
-## Generate reviews
-#python llava/eval/eval_gpt_review_bench.py \
-#    --question playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
-#    --context playground/data/eval/llava-bench-in-the-wild/context.jsonl \
-#    --rule llava/eval/table/rule.json \
-#    --answer-list \
-#        playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
-#        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL_NAME.jsonl \
-#    --output \
-#        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
-#
-## Summarize reviews
-#python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
+# Evaluate the model (2nd command - actual evaluation)
+python -m llava.eval.model_vqa \
+    --model-path ${SAVE_PATH} \
+    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
+    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/${MODEL_NAME}.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+
+# Create the reviews directory
+mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
+
+# Generate reviews
+python llava/eval/eval_gpt_review_bench.py \
+    --question playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --context playground/data/eval/llava-bench-in-the-wild/context.jsonl \
+    --rule llava/eval/table/rule.json \
+    --answer-list \
+        playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
+        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL_NAME.jsonl \
+    --output \
+        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
+
+# Summarize reviews
+python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
 
