096cae4cc689aa0b6c05dc14e5a9d27ed1266b4a
lwz <905328134@qq.com>
2025-01-13 18:43:53 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/mix_eval_llavabench.sh
+++ scripts/v1_5/m_train_domain_distill/mix_eval_llavabench.sh
@@ -19,29 +19,29 @@ python -m llava.eval.model_vqa_save_weight_hf_mixv3 \
   --mix-ratio ${MIX_RATIO} \
   --save-path ${SAVE_PATH}
 
-## Evaluate the model (2nd command - actual evaluation)
-#python -m llava.eval.model_vqa \
-#    --model-path ${SAVE_PATH} \
-#    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
-#    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
-#    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/${MODEL_NAME}.jsonl \
-#    --temperature 0 \
-#    --conv-mode vicuna_v1
-#
-#
-## Create the reviews directory
-#mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
-#
-## Generate reviews
-#python llava/eval/eval_gpt_review_bench.py \
-#    --question playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
-#    --context playground/data/eval/llava-bench-in-the-wild/context.jsonl \
-#    --rule llava/eval/table/rule.json \
-#    --answer-list \
-#        playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
-#        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL_NAME.jsonl \
-#    --output \
-#        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
-#
-## Summarize reviews
-#python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
+# Evaluate the model (2nd command - actual evaluation)
+python -m llava.eval.model_vqa \
+    --model-path ${SAVE_PATH} \
+    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
+    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/${MODEL_NAME}.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+
+# Create the reviews directory
+mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
+
+# Generate reviews
+python llava/eval/eval_gpt_review_bench.py \
+    --question playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --context playground/data/eval/llava-bench-in-the-wild/context.jsonl \
+    --rule llava/eval/table/rule.json \
+    --answer-list \
+        playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
+        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL_NAME.jsonl \
+    --output \
+        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
+
+# Summarize reviews
+python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL_NAME.jsonl
