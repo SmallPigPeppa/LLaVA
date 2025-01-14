13a0e8a2a803adab820058971e6ff5d563a67807
lwz <905328134@qq.com>
2025-01-14 14:52:02 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/mix_mme.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+export HF_HOME=/mnt/disk3/wzliu/huggingface
+export HF_HOME=/ppio_net0/huggingface
+export CUDA_VISIBLE_DEVICES=0
+
+
+MODEL_PATH_A="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0-merged"
+MODEL_PATH_B="ablation-ckpt/exp1-model-mix/llava-v1.5-7b-lora-coco2text-lambda1-merged"
+
+
+MIX_RATIO=0.5
+MODEL="${MODEL_PATH_B}-mix${MIX_RATIO}"
+
+rm -rf ./playground/data/eval/MME/eval_tool
+unzip ./playground/data/eval/MME/eval_tool.zip -d ./playground/data/eval/MME
+
+python -m llava.eval.model_vqa_loader \
+    --model-path $MODEL \
+    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
+    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
+    --answers-file ./playground/data/eval/MME/answers/llava-v1.5-13b.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+cd ./playground/data/eval/MME
+
+python convert_answer_to_mme.py --experiment llava-v1.5-13b
+
+cd eval_tool
+
+python calculation.py --results_dir answers/llava-v1.5-13b
+
+
+
+
