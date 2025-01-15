ea640f3fb80dbdf65ac3da84ca5b27ad92421537
lwz <905328134@qq.com>
2025-01-15 23:14:58 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme.sh
@@ -11,7 +11,7 @@ MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged"
 #MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged
 #MODEL=continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda0-merged
 #MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged"
-MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged-mix0.2-svdv3"
+#MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged-mix0.2-svdv3"
 
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0"
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda1.0"
