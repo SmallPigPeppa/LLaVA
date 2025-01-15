8b50e684f831c3df2e5c63e3c37169b7f10c4cd5
lwz <905328134@qq.com>
2025-01-15 22:08:03 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme.sh
@@ -10,9 +10,11 @@ MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 #MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged
 #MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged
 #MODEL=continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda0-merged
+MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged"
 
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0"
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda1.0"
+
 #MODEL="${MODEL_PATH}-merged"
 
 rm -rf ./playground/data/eval/MME/eval_tool
