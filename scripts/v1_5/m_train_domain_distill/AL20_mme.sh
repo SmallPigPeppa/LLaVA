09c964b9469820d1e3f89bab8e7b05c2dcdc4b54
lwz <905328134@qq.com>
2025-01-15 23:14:50 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme.sh
@@ -6,12 +6,12 @@ MODEL=checkpoints/llava-v1.5-7b-lora-merged
 MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged
 #MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 #MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged
-MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged
+MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged"
 #MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged
 #MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged
 #MODEL=continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda0-merged
 #MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged"
-#MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged-mix0.2-svdv3"
+MODEL="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-oinit-lambda1.0-merged-mix0.2-svdv3"
 
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-oinit-lambda1.0"
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda1.0"
