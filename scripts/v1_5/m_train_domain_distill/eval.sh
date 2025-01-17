be89c0a6da2caad044f93ab92d6f1ab6bdcfd14c
lwz <905328134@qq.com>
2025-01-17 19:10:16 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval.sh
+++ scripts/v1_5/m_train_domain_distill/eval.sh
@@ -3,25 +3,33 @@ export HF_HOME=/ppio_net0/huggingface
 export HF_HOME=/mnt/disk3/wzliu/huggingface
 export CUDA_VISIBLE_DEVICES=0
 
-MODEL_BASE="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 
-# First, evaluate the model without lambda
-#MODEL_PATH="continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill-lambda1.0-nodatamix"
-MODEL_PATH="continual-ckpt/distill/llava-v1.5-7b-lora-task-ocr-distill-lambda100.0-nodatamix-mse-all"
-OUT_PATH="${MODEL_PATH}-mix-merged"
-MODEL="${OUT_PATH##*/}"
+MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-cocoinit-lambda1.0"
+OUT_PATH="${MODEL_PATH}-merged"
+MODEL=$(basename $OUT_PATH)
 
-# Evaluate the model (1st command - saving weights)
-python -m llava.eval.model_vqa_save_weight_hf \
-  --model-path ${MODEL_PATH} \
-  --model-base ${MODEL_BASE} \
-  --save-path ${OUT_PATH}
 
 # Evaluate the model (2nd command - actual evaluation)
 python -m llava.eval.model_vqa \
-    --model-path "continual-ckpt/distill/$MODEL" \
+    --model-path ${OUT_PATH} \
     --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
     --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
-    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/${MODEL}.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
+# Create the reviews directory
+mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
+
+# Generate reviews
+python llava/eval/eval_gpt_review_bench.py \
+    --question playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+    --context playground/data/eval/llava-bench-in-the-wild/context.jsonl \
+    --rule llava/eval/table/rule.json \
+    --answer-list \
+        playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
+        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+    --output \
+        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL.jsonl
+
+# Summarize reviews
+python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL.jsonl
--- scripts/v1_5/m_train_domain_distill/llavabench.sh
+++ scripts/v1_5/m_train_domain_distill/llavabench.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 export HF_HOME=/ppio_net0/huggingface
+export HF_HOME=/mnt/disk3/wzliu/huggingface
 export CUDA_VISIBLE_DEVICES=0
-# Set model as a variable
-MODEL="llava-v1.5-7b-lora-task-ocr-merge-Amix"
-# Evaluate the model
+
+
+MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-ocr-cocoinit-lambda1.0"
+OUT_PATH="${MODEL_PATH}-merged"
+MODEL=$(basename $OUT_PATH)
+
+
+# Evaluate the model (2nd command - actual evaluation)
 python -m llava.eval.model_vqa \
-    --model-path "continual-ckpt/domain-Amix/$MODEL" \
+    --model-path ${OUT_PATH} \
     --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
     --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
-    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/${MODEL}.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
@@ -22,11 +28,9 @@ python llava/eval/eval_gpt_review_bench.py \
     --rule llava/eval/table/rule.json \
     --answer-list \
         playground/data/eval/llava-bench-in-the-wild/answers_gpt4.jsonl \
-        playground/data/eval/llava-bench-in-the-wild/answers/$MODEL.jsonl \
+        playground/data/eval/llava-bench-in-the-wild/answers/${MODEL}.jsonl \
     --output \
-        playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL.jsonl
+        playground/data/eval/llava-bench-in-the-wild/reviews/${MODEL}.jsonl
 
 # Summarize reviews
-python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/$MODEL.jsonl
-
-
+python llava/eval/summarize_gpt_review.py -f playground/data/eval/llava-bench-in-the-wild/reviews/${MODEL}.jsonl
