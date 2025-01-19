07d328b425d55f4335ec1f3f723b3da9bf0ab405
lwz <905328134@qq.com>
2025-01-20 00:42:33 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_iconvqa.sh
+++ scripts/v1_5/m_train_domain_distill/eval_iconvqa.sh
@@ -9,16 +9,16 @@ MODEL=$(basename $OUT_PATH)
 
 
 
-#python -m llava.eval.model_iconqa \
-#    --model-path ${MODEL_PATH} \
-#    --question-file ./playground/data/fine-tune/iconqa/val-1000.json \
-#    --image-folder ./playground/data \
-#    --answers-file ./playground/data/eval/iconvqa/answers/${MODEL}.jsonl \
-#    --temperature 0 \
-#    --conv-mode vicuna_v1
+python -m llava.eval.model_iconqa \
+    --model-path ${MODEL_PATH} \
+    --question-file ./playground/data/fine-tune/iconqa/val.json \
+    --image-folder ./playground/data \
+    --answers-file ./playground/data/eval/iconvqa/answers/${MODEL}.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
 
 
 python -m llava.eval.eval_iconqa \
-    --annotation-file ./playground/data/fine-tune/iconqa/val-1000.json \
+    --annotation-file ./playground/data/fine-tune/iconqa/val.json \
     --result-file ./playground/data/eval/iconvqa/answers/${MODEL}.jsonl \
     --output-dir ./playground/data/eval/iconvqa/output/${MODEL} \
