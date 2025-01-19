622a8b07e77483793a5b510f218a673826f5f9ba
lwz <905328134@qq.com>
2025-01-19 23:32:48 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/merge_iconqa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export HF_HOME=/ppio_net0/huggingface
+#export HF_HOME=/mnt/disk3/wzliu/huggingface
+export CUDA_VISIBLE_DEVICES=0
+
+
+MODEL_BASE="liuhaotian/llava-v1.5-7b"
+
+# First, evaluate the model without lambda
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa"
+
+OUT_PATH="${MODEL_PATH}-merged"
+
+# Evaluate the model (1st command - saving weights)
+python -m llava.eval.model_vqa_save_weight_hf \
+  --model-path ${MODEL_PATH} \
+  --model-base ${MODEL_BASE} \
+  --save-path ${OUT_PATH}
+
