e98898093a2bc95044209b097b872fde8428da27
lwz <905328134@qq.com>
2025-01-20 00:25:04 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/eval_iconvqa_1000.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export HF_HOME=/ppio_net0/huggingface
+#export HF_HOME=/mnt/disk3/wzliu/huggingface
+export CUDA_VISIBLE_DEVICES=0
+
+MODEL_PATH="liuhaotian/llava-v1.5-7b"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-merged"
+OUT_PATH="${MODEL_PATH}"
+MODEL=$(basename $OUT_PATH)
+
+
+
+#python -m llava.eval.model_iconqa \
+#    --model-path ${MODEL_PATH} \
+#    --question-file ./playground/data/fine-tune/iconqa/val-1000.json \
+#    --image-folder ./playground/data \
+#    --answers-file ./playground/data/eval/iconvqa/answers/${MODEL}.jsonl \
+#    --temperature 0 \
+#    --conv-mode vicuna_v1
+
+
+python -m llava.eval.eval_iconqa \
+    --annotation-file ./playground/data/fine-tune/iconqa/val-1000.json \
+    --result-file ./playground/data/eval/iconvqa/answers/${MODEL}.jsonl \
+    --output-dir ./playground/data/eval/iconvqa/output/${MODEL} \
