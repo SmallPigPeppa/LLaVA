314ef86d6f2124a4648e9c7533bcc0dfc1756a24
lwz <905328134@qq.com>
2025-01-19 23:15:35 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/mix_eval_llavabench_svdv3_all_vg.sh
+++ scripts/v1_5/m_train_domain_distill/mix_eval_llavabench_svdv3_all_vg.sh
@@ -2,22 +2,21 @@
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 
-
-
-
 MODEL_PATH_A1="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-gqa-svdv3-lambda1.0-merged-mix0.1-svdv3"
 MODEL_PATH_B1="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-vg-svdv3-lambda1.0-merged"
-
-
-# 固定的 SVD 保留比例
-SVD_RATIO=0.2
-SCALE_RATIO=0.5
-
 
 # 定义模型组合
 MODELS=(
     "$MODEL_PATH_A1 $MODEL_PATH_B1"
-#    "$MODEL_PATH_A2 $MODEL_PATH_B2"
+)
+
+# 定义比例列表
+MIX_RATIOS=(
+0.01
+0.02
+0.03
+0.04
+0.05
 )
 
 # 遍历每组模型
@@ -28,9 +27,8 @@ for MODEL_PAIR in "${MODELS[@]}"; do
 
     echo "Processing Model Pair: A=${MODEL_PATH_A}, B=${MODEL_PATH_B}"
 
-    # 遍历 MIX_RATIO 从 0 到 1
-    for i in $(seq 0.1 0.1 0.3); do
-        MIX_RATIO=$(printf "%.1f" $i)  # 保留一位小数
+    # 遍历 MIX_RATIO 列表
+    for MIX_RATIO in "${MIX_RATIOS[@]}"; do
         SAVE_PATH="${MODEL_PATH_B}-mix${MIX_RATIO}-svdv3"
         MODEL_NAME=$(basename $SAVE_PATH)
 
@@ -52,6 +50,3 @@ for MODEL_PAIR in "${MODELS[@]}"; do
 done
 
 echo "All evaluations completed."
-
-
-#/ppio_net0/code/openapi.sh stop ce8af4451be5be30
