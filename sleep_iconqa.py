1983a6cf4b41e991d9da177e34604a966898c0d1
lwz <905328134@qq.com>
2025-01-20 00:21:19 +0800

    initial

--- sleep_iconqa.py
+++ sleep_iconqa.py
@@ -3,14 +3,14 @@ import random
 
 # 读取原始数据文件
 input_file = '/ppio_net0/code/LLaVA/playground/data/fine-tune/iconqa/val.json'
-output_file = '/ppio_net0/code/LLaVA/playground/data/fine-tune/iconqa/val-1000.json'
+output_file = '/ppio_net0/code/LLaVA/playground/data/fine-tune/iconqa/val-2000.json'
 
 # 加载原始数据
 with open(input_file, 'r') as f:
     data = json.load(f)
 
 # 随机选择1000个样本
-sampled_data = random.sample(data, 1000)
+sampled_data = random.sample(data, 2000)
 
 # 保存为新的 JSON 文件
 with open(output_file, 'w') as f:
