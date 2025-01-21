7e6a18600dcd8e6f8a5ae58298a3aeca510c2061
lwz <905328134@qq.com>
2025-01-21 21:25:23 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/ft_eval_sciencaqa.sh
+++ scripts/v1_5/m_train_domain_distill/ft_eval_sciencaqa.sh
@@ -6,6 +6,7 @@ export HF_HOME=/ppio_net0/huggingface
 MODEL_PATH="continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-gqa-merged"
 MODEL_PATH="liuhaotian/llava-v1.5-7b"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-merged"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0-1w-merged"
 MODEL=$(basename $OUT_PATH)
 
 python -m llava.eval.model_vqa_science \
