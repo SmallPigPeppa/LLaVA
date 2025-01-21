fba6662bc6bd47b2628a82167f4dccb6a78e4ef5
lwz <905328134@qq.com>
2025-01-21 19:48:14 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task1_scienceqa_lambda1_2w.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Set Hugging Face cache directory
+export HF_HOME=/ppio_net0/huggingface
+#export HF_HOME=/mnt/disk3/wzliu/huggingface
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+
+VISION_TOWER="openai/clip-vit-large-patch14-336"
+MODEL_PATH="liuhaotian/llava-v1.5-7b"
+DATA_PATH="playground/data/fine-tune/ScienceQA/train-with-others-20000.json"
+OUTPUT_DIR="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0-2w"
+
+
+
+# Training command for OCR task
+deepspeed llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --distill True \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path ${MODEL_PATH} \
+    --version v1 \
+    --data_path ${DATA_PATH} \
+    --image_folder ./playground/data \
+    --vision_tower ${VISION_TOWER} \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ${OUTPUT_DIR} \
+    --num_train_epochs 1 \
+    --max_steps -1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
+
+#/ppio_net0/code/openapi.sh stop fcb0f576d11c01f5
+
