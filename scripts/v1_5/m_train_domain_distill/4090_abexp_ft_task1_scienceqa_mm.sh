4dd3948fc8387487668ffe02ce507133699fd7df
lwz <905328134@qq.com>
2025-01-22 01:08:09 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task1_scienceqa_mm.sh
+++ scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task1_scienceqa_mm.sh
@@ -8,8 +8,8 @@ export HF_HOME=/ppio_net0/huggingface
 
 VISION_TOWER="openai/clip-vit-large-patch14-336"
 MODEL_PATH="liuhaotian/llava-v1.5-7b"
-DATA_PATH="playground/data/fine-tune/ScienceQA/train.json"
-OUTPUT_DIR="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa"
+DATA_PATH="playground/data/fine-tune/ScienceQA/train_mm.json"
+OUTPUT_DIR="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-mm"
 
 
 
