3a58a7df4462ea02b127e0866bfd195f2462f916
lwz <905328134@qq.com>
2025-01-21 23:26:07 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme_domain.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme_domain.sh
@@ -6,15 +6,15 @@ tmux set-option history-limit 1000000
 
 # List of model paths
 MODELS=(
-#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged"
-#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
-#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged"
-#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged"
-#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged"
-#    "continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged"
-     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-ocr-merged"
-     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-textvqa-merged"
-     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-gqa-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged"
+    "continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged"
+#     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-ocr-merged"
+#     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-textvqa-merged"
+#     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-gqa-merged"
 #     "continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-vg-merged"
 )
 
