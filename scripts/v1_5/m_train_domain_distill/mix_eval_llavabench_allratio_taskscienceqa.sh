5c19e6e15e57ff46751499d7f8cc5d9b93586899
lwz <905328134@qq.com>
2025-01-21 18:08:08 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/mix_eval_llavabench_allratio_taskscienceqa.sh
+++ scripts/v1_5/m_train_domain_distill/mix_eval_llavabench_allratio_taskscienceqa.sh
@@ -18,15 +18,15 @@ mkdir -p $REVIEWS_DIR
 # List of mix_ratios to evaluate
 MIX_RATIOS=(
 #  0.0
-  0.1
+#  0.1
   0.2
-  0.3
-  0.4
-  0.5
-  0.6
-  0.7
-  0.8
-  0.9
+#  0.3
+#  0.4
+#  0.5
+#  0.6
+#  0.7
+#  0.8
+#  0.9
 #  1.0
 )
 
