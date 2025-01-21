109c04717779f2e73d14ef86b0e902b55225518a
lwz <905328134@qq.com>
2025-01-21 17:22:15 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/ft_eval_sciencaqa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#export HF_HOME=/mnt/disk3/wzliu/huggingface
+export HF_HOME=/ppio_net0/huggingface
+#export CUDA_VISIBLE_DEVICES=0
+
+MODEL_PATH="continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-gqa-merged"
+MODEL_PATH="liuhaotian/llava-v1.5-7b"
+MODEL=$(basename $OUT_PATH)
+
+python -m llava.eval.model_vqa_science \
+    --model-path ${MODEL_PATH} \
+    --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
+    --image-folder ./playground/data/eval/scienceqa/images/test \
+    --answers-file ./playground/data/eval/scienceqa/answers/${MODEL}.jsonl \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+python llava/eval/eval_science_qa.py \
+    --base-dir ./playground/data/eval/scienceqa \
+    --result-file ./playground/data/eval/scienceqa/answers/${MODEL}.jsonl \
+    --output-file ./playground/data/eval/scienceqa/answers/${MODEL}_output.jsonl \
+    --output-result ./playground/data/eval/scienceqa/answers/${MODEL}_result.json
