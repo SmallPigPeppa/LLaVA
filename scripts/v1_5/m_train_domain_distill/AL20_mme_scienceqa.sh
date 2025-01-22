b70a1627e4616375e0c1211674f29a0f3688123b
lwz <905328134@qq.com>
2025-01-22 12:54:42 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme_scienceqa.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme_scienceqa.sh
@@ -7,6 +7,7 @@ MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-lambda1.0"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0-1w"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-mm"
 
 MODEL="${MODEL_PATH}-merged"
 
