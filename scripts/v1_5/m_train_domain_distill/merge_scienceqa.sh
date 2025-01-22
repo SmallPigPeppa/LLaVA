58367c618cee2c6c0a906b5ea49d39b9723b00c0
lwz <905328134@qq.com>
2025-01-22 12:54:07 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/merge_scienceqa.sh
+++ scripts/v1_5/m_train_domain_distill/merge_scienceqa.sh
@@ -12,6 +12,7 @@ MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0-1w"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-mm"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-scienceqa-lambda1.0-1w-2"
 
 OUT_PATH="${MODEL_PATH}-merged"
 
