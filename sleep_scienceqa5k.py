378ca464f15d8275ec34fa5449a44f7d5938fd2a
lwz <905328134@qq.com>
2025-01-22 13:52:59 +0800

    initial

--- sleep_scienceqa5k.py
+++ sleep_scienceqa5k.py
@@ -2,7 +2,7 @@ import json
 import random
 
 # Load OCR data
-with open('playground/data/fine-tune/ScienceQA/train.json', 'r') as f:
+with open('playground/data/fine-tune/ScienceQA/train-mm.json', 'r') as f:
     ocr_data = json.load(f)
 
 # Load others data
@@ -19,7 +19,7 @@ merged_data = ocr_data + selected_others_data
 random.shuffle(merged_data)
 
 # Save the merged and shuffled data to a new JSON file
-with open('playground/data/fine-tune/ScienceQA/train-with-others-5k.json', 'w') as f:
+with open('playground/data/fine-tune/ScienceQA/train-mm-with-others-5k.json', 'w') as f:
     json.dump(merged_data, f, ensure_ascii=False, indent=4)
 
 print(f"合并成功，共生成 {len(merged_data)} 个样本。")
