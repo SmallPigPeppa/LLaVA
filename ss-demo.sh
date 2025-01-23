b5c36d76e72a5a4ccaf95e7bdc438ced2d862330
lwz <905328134@qq.com>
2025-01-23 15:33:59 +0800

    initial

--- ss-demo.sh
+++ ss-demo.sh
@@ -4,6 +4,11 @@ MODEL_PATH="continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-ocr-merged"
 #MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
 #MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged"
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda1.0-merged"
+#python -m llava.serve.cli \
+#    --model-path  $MODEL_PATH\
+#    --image-file "demo/020.jpg"
+
+
 python -m llava.serve.cli \
     --model-path  $MODEL_PATH\
-    --image-file "demo/020.jpg"
+    --image-file "demo/011341143X.jpg"
