76440b6f9752010ce412060f0ea783b841678e40
lwz <905328134@qq.com>
2025-01-23 15:22:27 +0800

    initial

--- ss-demo.sh
+++ ss-demo.sh
@@ -2,7 +2,7 @@ export HF_HOME=/ppio_net0/huggingface
 MODEL_PATH="continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-ocr-merged"
 #MODEL_PATH="liuhaotian/llava-v1.5-7b"
 #MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
-MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr-merged"
+MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged"
 #MODEL_PATH="continual-ckpt/domain-incremental-mse/llava-v1.5-7b-lora-task-coco-v4-lambda1.0-merged"
 python -m llava.serve.cli \
     --model-path  $MODEL_PATH\
