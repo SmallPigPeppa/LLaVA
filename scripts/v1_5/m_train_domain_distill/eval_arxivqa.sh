12f26bf9f380f6b81750545674f001ad563773ce
lwz <905328134@qq.com>
2025-01-26 00:03:32 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/eval_arxivqa.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#!/bin/bash
+export HF_HOME=/ppio_net0/huggingface
+export CUDA_VISIBLE_DEVICES=0
+
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-merged"
+OUT_PATH="${MODEL_PATH}"
+MODEL=$(basename $OUT_PATH)
+
+
+python -m llava.eval.model_arxivqa \
+    --model-path ${MODEL_PATH} \
+    --question-file ./playground/data/fine-tune/ArxivQA/test_1w.json \
+    --image-folder ./playground/data \
+    --answers-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+
+python -m llava.eval.eval_arxivqa \
+    --annotation-file ./playground/data/fine-tune/ArxivQA/test_1w.json \
+    --result-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
+    --output-dir ./playground/data/eval/arxivqa/answers/${MODEL} \
+
