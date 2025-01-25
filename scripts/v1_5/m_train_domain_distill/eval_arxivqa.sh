73ece73c3e9444a9591993c9a6e0b8a211cf741e
lwz <905328134@qq.com>
2025-01-26 00:57:49 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_arxivqa.sh
+++ scripts/v1_5/m_train_domain_distill/eval_arxivqa.sh
@@ -4,14 +4,15 @@
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-merged"
+MODEL_PATH="liuhaotian/llava-v1.5-7b"
+#MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-merged"
 OUT_PATH="${MODEL_PATH}"
 MODEL=$(basename $OUT_PATH)
 
 
 python -m llava.eval.model_arxivqa \
     --model-path ${MODEL_PATH} \
-    --question-file ./playground/data/fine-tune/ArxivQA/test_1w.json \
+    --question-file ./playground/data/fine-tune/ArxivQA/test_2k.json \
     --image-folder ./playground/data \
     --answers-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
     --temperature 0 \
@@ -19,7 +20,7 @@ python -m llava.eval.model_arxivqa \
 
 
 python -m llava.eval.eval_arxivqa \
-    --annotation-file ./playground/data/fine-tune/ArxivQA/test_1w.json \
+    --annotation-file ./playground/data/fine-tune/ArxivQA/test_2k.json \
     --result-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
     --output-dir ./playground/data/eval/arxivqa/answers/${MODEL} \
 
