919002bc70d18b36d9a6f9ceb7b2d8a0272f7afb
lwz <905328134@qq.com>
2025-01-26 00:09:28 +0800

    initial

--- sleep_arxivqa_val.py
+++ sleep_arxivqa_val.py
@@ -10,10 +10,11 @@ with open(input_file, 'r') as f:
     data = json.load(f)
 
 # 随机选择1000个样本
-sampled_data = random.sample(data, 2000)
+k=2000
+sampled_data = random.sample(data, k)
 
 # 保存为新的 JSON 文件
 with open(output_file, 'w') as f:
     json.dump(sampled_data, f, indent=4)
 
-print(f"Successfully selected 1000 samples and saved to {output_file}.")
+print(f"Successfully selected {k} samples and saved to {output_file}.")
