19dec2be5bb8474b80a430cf9d771b2ddeac0fad
lwz <905328134@qq.com>
2025-01-26 00:32:04 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_arxivqa.sh
+++ scripts/v1_5/m_train_domain_distill/eval_arxivqa.sh
@@ -9,17 +9,17 @@ OUT_PATH="${MODEL_PATH}"
 MODEL=$(basename $OUT_PATH)
 
 
-#python -m llava.eval.model_arxivqa \
-#    --model-path ${MODEL_PATH} \
-#    --question-file ./playground/data/fine-tune/ArxivQA/test_2k.json \
-#    --image-folder ./playground/data \
-#    --answers-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
-#    --temperature 0 \
-#    --conv-mode vicuna_v1
+python -m llava.eval.model_arxivqa \
+    --model-path ${MODEL_PATH} \
+    --question-file ./playground/data/fine-tune/ArxivQA/test_1w.json \
+    --image-folder ./playground/data \
+    --answers-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
 
 
 python -m llava.eval.eval_arxivqa \
-    --annotation-file ./playground/data/fine-tune/ArxivQA/test_2k.json \
+    --annotation-file ./playground/data/fine-tune/ArxivQA/test_1w.json \
     --result-file ./playground/data/eval/arxivqa/answers/${MODEL}.jsonl \
     --output-dir ./playground/data/eval/arxivqa/answers/${MODEL} \
 
