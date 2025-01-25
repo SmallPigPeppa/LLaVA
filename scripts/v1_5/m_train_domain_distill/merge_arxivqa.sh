b8de1bd41de7252edeae27f521ed323c5b7c65ce
lwz <905328134@qq.com>
2025-01-25 23:46:21 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/merge_arxivqa.sh
+++ scripts/v1_5/m_train_domain_distill/merge_arxivqa.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-export HF_HOME=/ppio_net0/huggingface
-#export HF_HOME=/mnt/disk3/wzliu/huggingface
-export CUDA_VISIBLE_DEVICES=0
 
+export HF_HOME=/ppio_net0/huggingface
+export CUDA_VISIBLE_DEVICES=0
 
 MODEL_BASE="liuhaotian/llava-v1.5-7b"
 
-# First, evaluate the model without lambda
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa"
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-lambda1.0"
+# List of model paths to evaluate
+MODEL_PATHS=(
+  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa"
+  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-lambda1.0"
+)
 
-OUT_PATH="${MODEL_PATH}-merged"
+# Loop through each model path
+for MODEL_PATH in "${MODEL_PATHS[@]}"; do
+  OUT_PATH="${MODEL_PATH}-merged"
 
-# Evaluate the model (1st command - saving weights)
-python -m llava.eval.model_vqa_save_weight_hf \
-  --model-path ${MODEL_PATH} \
-  --model-base ${MODEL_BASE} \
-  --save-path ${OUT_PATH}
-
+  # Evaluate the model (save weights)
+  echo "Evaluating model: ${MODEL_PATH}"
+  python -m llava.eval.model_vqa_save_weight_hf \
+    --model-path ${MODEL_PATH} \
+    --model-base ${MODEL_BASE} \
+    --save-path ${OUT_PATH}
+done
