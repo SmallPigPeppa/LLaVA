4c77830d9aa47b5fcd323d6d54e31bc174ba5d37
lwz <905328134@qq.com>
2025-01-26 01:20:23 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task1_figure_lambda1.sh
+++ scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task1_figure_lambda1.sh
@@ -2,21 +2,20 @@
 
 # Set Hugging Face cache directory
 export HF_HOME=/ppio_net0/huggingface
-#export HF_HOME=/mnt/disk3/wzliu/huggingface
 #export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 
 VISION_TOWER="openai/clip-vit-large-patch14-336"
 MODEL_PATH="liuhaotian/llava-v1.5-7b"
-DATA_PATH="playground/data/fine-tune/ArxivQA/train_4w.json"
-OUTPUT_DIR="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super"
+DATA_PATH="playground/data/fine-tune/FigureQA/train-with-others.json"
+OUTPUT_DIR="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-lambda1.0"
 
 
 
 # Training command for OCR task
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --distill False \
+    --distill True \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path ${MODEL_PATH} \
     --version v1 \
