7e946649da52a9b99c80a8faa2f5149c7522e590
lwz <905328134@qq.com>
2025-01-26 17:24:09 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/merge_figureqa.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export HF_HOME=/ppio_net0/huggingface
+export CUDA_VISIBLE_DEVICES=0
+
+MODEL_BASE="liuhaotian/llava-v1.5-7b"
+
+# List of model paths to evaluate
+MODEL_PATHS=(
+#  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa"
+#  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-lambda1.0"
+  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa"
+  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-lambda1.0"
+)
+
+# Loop through each model path
+for MODEL_PATH in "${MODEL_PATHS[@]}"; do
+  OUT_PATH="${MODEL_PATH}-merged"
+
+  # Evaluate the model (save weights)
+  echo "Evaluating model: ${MODEL_PATH}"
+  python -m llava.eval.model_vqa_save_weight_hf \
+    --model-path ${MODEL_PATH} \
+    --model-base ${MODEL_BASE} \
+    --save-path ${OUT_PATH}
+done
