f5e1aab45ccbccd7daafdf2c75c08b9c2530af15
lwz <905328134@qq.com>
2025-01-26 17:40:26 +0800

    initial

---
+++ scripts/v1_5/m_train_domain_distill/merge_super.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export HF_HOME=/ppio_net0/huggingface
+export CUDA_VISIBLE_DEVICES=0
+
+MODEL_BASE="liuhaotian/llava-v1.5-7b"
+
+# List of model paths to evaluate
+MODEL_PATHS=(
+  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super"
+  "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super-lambda1.0"
+)
+
+# Loop through each model path
+for MODEL_PATH in "${MODEL_PATHS[@]}"; do
+  OUT_PATH="${MODEL_PATH}-merged"
+
+  # Evaluate the model (save weights)
+  echo "Evaluating model: ${MODEL_PATH}"
+  python -m llava.eval.model_vqa_save_weight_hf \
+    --model-path ${MODEL_PATH} \
+    --model-base ${MODEL_BASE} \
+    --save-path ${OUT_PATH}
+done
