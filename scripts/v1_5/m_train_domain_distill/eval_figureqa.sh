ac0c05b35318a235e432261f573876870bd72990
lwz <905328134@qq.com>
2025-01-26 16:53:18 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_figureqa.sh
+++ scripts/v1_5/m_train_domain_distill/eval_figureqa.sh
@@ -4,7 +4,8 @@
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-merged"
+MODEL_PATH="liuhaotian/llava-v1.5-7b"
+#MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-merged"
 OUT_PATH="${MODEL_PATH}"
 MODEL=$(basename $OUT_PATH)
 
@@ -12,13 +13,13 @@ MODEL=$(basename $OUT_PATH)
 
 python -m llava.eval.model_figureqa \
     --model-path ${MODEL_PATH} \
-    --question-file ./playground/data/fine-tune/FigureQA/test_2k.json \
+    --question-file ./playground/data/fine-tune/FigureQA/test.json \
     --image-folder ./playground/data \
     --answers-file ./playground/data/eval/figureqa/answers/${MODEL}.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python -m llava.eval.eval_figureqa \
-    --annotation-file ./playground/data/fine-tune/FigureQA/test_2k.json \
+    --annotation-file ./playground/data/fine-tune/FigureQA/test.json \
     --result-file ./playground/data/eval/figureqa/answers/${MODEL}.jsonl \
     --output-dir ./playground/data/eval/figureqa/answers/${MODEL} \
