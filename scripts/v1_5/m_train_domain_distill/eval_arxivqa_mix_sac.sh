12fa8dc11f516808ab39be0ca2add51c84a512f0
lwz <905328134@qq.com>
2025-01-26 20:11:27 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_arxivqa_mix_sac.sh
+++ scripts/v1_5/m_train_domain_distill/eval_arxivqa_mix_sac.sh
@@ -5,17 +5,17 @@ export CUDA_VISIBLE_DEVICES=0
 
 # 定义模型路径
 MODEL_PATH="liuhaotian/llava-v1.5-7b"
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-merged"
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-lambda1.0-merged"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-merged"
+#MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-lambda1.0-merged"
 
 # 定义Mix率的不同值
 MIX_RATIOS=(
 #  0.0
   0.1
   0.2
-  0.3
-  0.4
-  0.5
+#  0.3
+#  0.4
+#  0.5
 #  0.6
 #  0.7
 #  0.8
