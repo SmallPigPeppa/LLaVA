b267b050911fa048f91e1d12f7ab569b3af332cd
lwz <905328134@qq.com>
2025-01-27 19:34:42 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_seed.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_seed.sh
@@ -21,29 +21,29 @@ MODEL_PATH="checkpoints/llava-v1.5-7b-lora-merged"
 MODEL_PATH="continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged"
 MODEL_NAME=$(basename ${MODEL_PATH})
 
-#for IDX in $(seq 0 $((CHUNKS-1))); do
-#    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
-#        --model-path ${MODEL_PATH} \
-#        --question-file ./playground/data/eval/seed_bench/llava-seed-bench-modified.jsonl \
-#        --image-folder ./playground/data/eval/seed_bench \
-#        --answers-file ./playground/data/eval/seed_bench/answers/${MODEL_NAME}/${CHUNKS}_${IDX}.jsonl \
-#        --num-chunks $CHUNKS \
-#        --chunk-idx $IDX \
-#        --temperature 0 \
-#        --conv-mode vicuna_v1 &
-#done
-#
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
+        --model-path ${MODEL_PATH} \
+        --question-file ./playground/data/eval/seed_bench/llava-seed-bench-modified.jsonl \
+        --image-folder ./playground/data/eval/seed_bench \
+        --answers-file ./playground/data/eval/seed_bench/answers/${MODEL_NAME}/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --conv-mode vicuna_v1 &
+done
+
 #wait
-#
+
 output_file=./playground/data/eval/seed_bench/answers/$CKPT/merge.jsonl
-#
-## Clear out the output file if it exists.
-#> "$output_file"
-#
-## Loop through the indices and concatenate each file.
-#for IDX in $(seq 0 $((CHUNKS-1))); do
-#    cat ./playground/data/eval/seed_bench/answers/${MODEL_NAME}/${CHUNKS}_${IDX}.jsonl >> "$output_file"
-#done
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ./playground/data/eval/seed_bench/answers/${MODEL_NAME}/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
 
 # Evaluate
 python scripts/convert_seed_for_submission.py \
