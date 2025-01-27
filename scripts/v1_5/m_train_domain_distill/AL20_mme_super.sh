7d8ef893455c97db291eacaf2dcee23f04380814
lwz <905328134@qq.com>
2025-01-27 23:46:22 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme_super.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme_super.sh
@@ -8,7 +8,7 @@ MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super-lambda1.0"
 
 MODEL="${MODEL_PATH}-merged"
 
-#MODEL="liuhaotian/llava-v1.5-7b"
+MODEL_NAME=$(basename ${MODEL_PATH})
 
 rm -rf ./playground/data/eval/MME/eval_tool
 unzip ./playground/data/eval/MME/eval_tool.zip -d ./playground/data/eval/MME
@@ -17,17 +17,17 @@ python -m llava.eval.model_vqa_loader \
     --model-path $MODEL \
     --question-file ./playground/data/eval/MME/llava_mme.jsonl \
     --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
-    --answers-file ./playground/data/eval/MME/answers/llava-v1.5-13b.jsonl \
+    --answers-file ./playground/data/eval/MME/answers/${MODEL_NAME}.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 cd ./playground/data/eval/MME
 
-python convert_answer_to_mme.py --experiment llava-v1.5-13b
+python convert_answer_to_mme.py --experiment ${MODEL_NAME}
 
 cd eval_tool
 
-python calculation.py --results_dir answers/llava-v1.5-13b
+python calculation.py --results_dir answers/${MODEL_NAME}
 
 
 
