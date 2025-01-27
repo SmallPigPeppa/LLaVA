64753ffc6c71d8b10f1e4b0fa9e0eb11e9eb81b2
lwz <905328134@qq.com>
2025-01-28 02:15:32 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme_super.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme_super.sh
@@ -2,25 +2,24 @@
 export HF_HOME=/mnt/disk3/wzliu/huggingface
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
-MODEL="checkpoints/llava-v1.5-7b-lora-merged"
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa"
-MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super-lambda1.0"
+MODEL_PATH="checkpoints/llava-v1.5-7b-lora-merged"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-merged"
+MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super-lambda1.0-merged"
 
-MODEL="${MODEL_PATH}-merged"
-MODEL="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task1-iconqa-task2-super-merged"
+MODEL_PATH="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task1-iconqa-task2-super-merged"
 
 MODEL_NAME=$(basename ${MODEL_PATH})
 
 rm -rf ./playground/data/eval/MME/eval_tool
 unzip ./playground/data/eval/MME/eval_tool.zip -d ./playground/data/eval/MME
 
-#python -m llava.eval.model_vqa_loader \
-#    --model-path $MODEL \
-#    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
-#    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
-#    --answers-file ./playground/data/eval/MME/answers/${MODEL_NAME}.jsonl \
-#    --temperature 0 \
-#    --conv-mode vicuna_v1
+python -m llava.eval.model_vqa_loader \
+    --model-path ${MODEL_PATH} \
+    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
+    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
+    --answers-file ./playground/data/eval/MME/answers/${MODEL_NAME}.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
 
 cd ./playground/data/eval/MME
 
