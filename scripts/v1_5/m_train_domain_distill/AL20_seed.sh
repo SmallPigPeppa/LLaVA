7d2fb6f96fa6d19fe6c64fa3b8d6a7343c095f79
lwz <905328134@qq.com>
2025-01-27 20:20:38 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_seed.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_seed.sh
@@ -39,7 +39,7 @@ OUT_FILE="./playground/data/eval/seed_bench/answers/${MODEL_NAME}/merge.jsonl"
 
 
 # Clear out the output file if it exists.
-> "$output_file"
+> ${OUT_FILE}
 
 # Loop through the indices and concatenate each file.
 for IDX in $(seq 0 $((CHUNKS-1))); do
