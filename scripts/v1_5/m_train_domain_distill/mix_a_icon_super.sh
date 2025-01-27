9f6eb77cac00d045a6a5f62f65c76dfcf0752037
lwz <905328134@qq.com>
2025-01-28 01:58:49 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/mix_a_icon_super.sh
+++ scripts/v1_5/m_train_domain_distill/mix_a_icon_super.sh
@@ -2,12 +2,12 @@
 export HF_HOME=/ppio_net0/huggingface
 export CUDA_VISIBLE_DEVICES=0
 
-MODEL_PATH_A="finetune-ckpt/fine-tune/"
-MODEL_PATH_B="finetune-ckpt/fine-tune/"
+MODEL_PATH_A="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-lambda1.0-merged-mix0.6-svdv3"
+MODEL_PATH_B="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super-lambda1.0-merged-mix0.6-svdv3"
 
 
 MIX_RATIO=0.5
-SAVE_PATH="${MODEL_PATH_B}-mix${MIX_RATIO}"
+SAVE_PATH="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task1-iconqa-task2-super-merged"
 
 
 # Evaluate the model (1st command - saving weights)
