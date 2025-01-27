25623e76659be2a145d9a545ee111f584a9ae4cd
lwz <905328134@qq.com>
2025-01-28 00:49:25 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/AL20_mme_textvqa.sh
+++ scripts/v1_5/m_train_domain_distill/AL20_mme_textvqa.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0
 MODEL="checkpoints/llava-v1.5-7b-lora-merged"
 MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-others-merged"
 MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-coco-merged"
-MODEL=continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged
+MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-ocr_vqa-merged"
 MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-textvqa-merged"
 #MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-gqa-merged"
 #MODEL="continual-ckpt/domain/llava-v1.5-7b-lora-task-vg-merged"
@@ -24,11 +24,11 @@ python -m llava.eval.model_vqa_loader \
 
 cd ./playground/data/eval/MME
 
-python convert_answer_to_mme.py --experiment llava-v1.5-13b
+python convert_answer_to_mme_new.py --experiment ${MODEL_NAME}
 
 cd eval_tool
 
-python calculation.py --results_dir answers/llava-v1.5-13b
+python calculation.py --results_dir answers/${MODEL_NAME}
 
 
 
