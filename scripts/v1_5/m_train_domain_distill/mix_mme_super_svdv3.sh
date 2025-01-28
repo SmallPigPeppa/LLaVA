d8576fa01bc1b879627902f4e528737d6efc0219
lwz <905328134@qq.com>
2025-01-28 09:20:59 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/mix_mme_super_svdv3.sh
+++ scripts/v1_5/m_train_domain_distill/mix_mme_super_svdv3.sh
@@ -7,6 +7,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 #MODEL_PATH_B="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-arxivqa-merged"
 MODEL_PATH_B="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-super-lambda1.0-merged"
+MODEL_PATH_B="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task2-super-lambda1.0-merged"
 
 # 定义所有的 mix_ratios
 MIX_RATIOS=(
