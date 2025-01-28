6a1a1c4abc6b5248087b98fd1b61b1482ee2f8d7
lwz <905328134@qq.com>
2025-01-28 21:02:36 +0800

    initial

--- scripts/v1_5/m_train_domain/generate_Aold_multi_data_task_math.sh
+++ scripts/v1_5/m_train_domain/generate_Aold_multi_data_task_math.sh
@@ -48,7 +48,7 @@ done
 echo "Running evaluations on each GPU..."
 for i in $(seq 0 $((NUM_GPUS - 1)))
 do
-    CUDA_VISIBLE_DEVICES=$i python -m llava.eval.model_vqa_generate_Aold \
+    CUDA_VISIBLE_DEVICES=$i python -m llava.eval.model_vqa_generate_Aold_1qa \
         --model-path ${MODEL_PATH} \
         --image-folder ./playground/data \
         --dataset-file "$tmp_dir/part_$((i + 1)).json" \
