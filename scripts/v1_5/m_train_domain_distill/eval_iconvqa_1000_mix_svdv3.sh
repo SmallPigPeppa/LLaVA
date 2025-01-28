6808684ec64b672c9d84145dbe461f984dbee720
lwz <905328134@qq.com>
2025-01-28 19:12:04 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_iconvqa_1000_mix_svdv3.sh
+++ scripts/v1_5/m_train_domain_distill/eval_iconvqa_1000_mix_svdv3.sh
@@ -8,19 +8,20 @@ MODEL_PATH="liuhaotian/llava-v1.5-7b"
 MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-merged"
 #MODEL_PATH="finetune-ckpt/fine-tune/llava-v1.5-7b-lora-iconqa-lambda1.0-merged"
 MODEL_PATH="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task2-super-lambda1.0-merged"
+MODEL_PATH="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task3-math-lambda1.0-merged"
 
 # 定义Mix率的不同值
 MIX_RATIOS=(
 #  0.0
-#  0.1
+  0.1
 #  0.125
-  0.11
-  0.12
-  0.13
-#  0.2
-#  0.3
-#  0.4
-#  0.5
+#  0.11
+#  0.12
+#  0.13
+  0.2
+  0.3
+  0.4
+  0.5
 #  0.6
 #  0.7
 #  0.8
