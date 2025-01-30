bd73f412d7c2bfb9aaa40c9d4173de82e6a32cdb
lwz <905328134@qq.com>
2025-01-30 12:16:32 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task5_arxivqa_lambda1.sh
+++ scripts/v1_5/m_train_domain_distill/4090_abexp_ft_task5_arxivqa_lambda1.sh
@@ -14,43 +14,54 @@ OUTPUT_DIR="finetune-ckpt/llava-c/llava-v1.5-7b-lora-task5-arxiv-lambda1.0"
 
 
 # Training command for OCR task
-deepspeed llava/train/train_mem_mse.py \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --distill True \
-    --deepspeed ./scripts/zero3.json \
-    --model_name_or_path ${MODEL_PATH} \
-    --version v1 \
-    --data_path ${DATA_PATH} \
-    --image_folder ./playground/data \
-    --vision_tower ${VISION_TOWER} \
-    --mm_projector_type mlp2x_gelu \
-    --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
-    --group_by_modality_length True \
-    --bf16 True \
-    --output_dir ${OUTPUT_DIR} \
-    --num_train_epochs 1 \
-    --max_steps -1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 4 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 50000 \
-    --save_total_limit 1 \
-    --learning_rate 2e-4 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 True \
-    --model_max_length 2048 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-    --report_to wandb
+#deepspeed llava/train/train_mem_mse.py \
+#    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+#    --distill True \
+#    --deepspeed ./scripts/zero3.json \
+#    --model_name_or_path ${MODEL_PATH} \
+#    --version v1 \
+#    --data_path ${DATA_PATH} \
+#    --image_folder ./playground/data \
+#    --vision_tower ${VISION_TOWER} \
+#    --mm_projector_type mlp2x_gelu \
+#    --mm_vision_select_layer -2 \
+#    --mm_use_im_start_end False \
+#    --mm_use_im_patch_token False \
+#    --image_aspect_ratio pad \
+#    --group_by_modality_length True \
+#    --bf16 True \
+#    --output_dir ${OUTPUT_DIR} \
+#    --num_train_epochs 1 \
+#    --max_steps -1 \
+#    --per_device_train_batch_size 4 \
+#    --per_device_eval_batch_size 1 \
+#    --gradient_accumulation_steps 4 \
+#    --evaluation_strategy "no" \
+#    --save_strategy "steps" \
+#    --save_steps 50000 \
+#    --save_total_limit 1 \
+#    --learning_rate 2e-4 \
+#    --weight_decay 0. \
+#    --warmup_ratio 0.03 \
+#    --lr_scheduler_type "cosine" \
+#    --logging_steps 1 \
+#    --tf32 True \
+#    --model_max_length 2048 \
+#    --gradient_checkpointing True \
+#    --dataloader_num_workers 4 \
+#    --lazy_preprocess True \
+#    --report_to wandb
 
-/ppio_net0/code/openapi.sh stop a63cc4ad4ec4c1d3
+
+MERGE_PATH="${OUTPUT_DIR}-merged"
+
+# Evaluate the model (save weights)
+echo "Evaluating model: ${MODEL_PATH}"
+python -m llava.eval.model_vqa_save_weight_hf \
+  --model-path ${OUTPUT_DIR} \
+  --model-base ${MODEL_PATH} \
+  --save-path ${MERGE_PATH}
+
+
+#/ppio_net0/code/openapi.sh stop a63cc4ad4ec4c1d3
 
