01ba0387ad64659ead48425c0e8b6c38aaf4e3c9
lwz <905328134@qq.com>
2025-01-30 13:25:05 +0800

    initial

--- scripts/v1_5/m_train_domain_distill/eval_figureqa.sh
+++ scripts/v1_5/m_train_domain_distill/eval_figureqa.sh
@@ -5,8 +5,9 @@ export CUDA_VISIBLE_DEVICES=0
 # List of model paths to evaluate
 MODEL_PATHS=(
 #    "liuhaotian/llava-v1.5-7b"
-    "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-merged"
-    "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-lambda1.0-merged"
+#    "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-merged"
+#    "finetune-ckpt/fine-tune/llava-v1.5-7b-lora-figureqa-lambda1.0-merged"
+    "finetune-ckpt/llava-c/llava-v1.5-7b-lora-task4-figureqa-lambda1.0-merged-mix0.13-svdv3"
 )
 
 # Iterate through each model path
@@ -17,13 +18,13 @@ for MODEL_PATH in "${MODEL_PATHS[@]}"; do
     echo "Evaluating model: ${MODEL_PATH}"
 
     # Step 1: Run model evaluation for FigureQA
-    python -m llava.eval.model_figureqa \
-        --model-path ${MODEL_PATH} \
-        --question-file ./playground/data/fine-tune/FigureQA/test_2k.json \
-        --image-folder ./playground/data \
-        --answers-file ./playground/data/eval/figureqa/answers/${MODEL}.jsonl \
-        --temperature 0 \
-        --conv-mode vicuna_v1
+#    python -m llava.eval.model_figureqa \
+#        --model-path ${MODEL_PATH} \
+#        --question-file ./playground/data/fine-tune/FigureQA/test_2k.json \
+#        --image-folder ./playground/data \
+#        --answers-file ./playground/data/eval/figureqa/answers/${MODEL}.jsonl \
+#        --temperature 0 \
+#        --conv-mode vicuna_v1
 
     # Step 2: Evaluate results
     python -m llava.eval.eval_figureqa \
