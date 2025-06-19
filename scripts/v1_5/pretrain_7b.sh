bba41c8316a0c72c96d68cc8b6436a3cb66af6aa
lwz <905328134@qq.com>
2025-06-19 16:56:12 +0800

    initial

--- scripts/v1_5/pretrain_7b.sh
+++ scripts/v1_5/pretrain_7b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export MASTER_PORT=29501
+MASTER_ADDR=127.0.0.1 MASTER_PORT=29501 \
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
