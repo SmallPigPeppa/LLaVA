1a1cd92afcb8baa76a86cf6ded45f3ab503913c2
lwz <905328134@qq.com>
2025-06-20 01:57:13 +0800

    initial

--- scripts/v1_5/m_train/finetune_7b.sh
+++ scripts/v1_5/m_train/finetune_7b.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-export HF_HOME=/ppio_net0/huggingface
-
 deepspeed llava/train/train_mem.py \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path ./checkpoints/llava-v1.5-7b-lora-x \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./playground/data/debug/llava_v1_5_mix665k-random-100.json \
-    --image_folder ./playground/data \
+    --data_path /mnt/hdfs/byte_content_security/user/liuwenzhuo/datasets/llava665k/llava_v1_5_mix665k.json \
+    --image_folder /mnt/hdfs/byte_content_security/user/liuwenzhuo/datasets/llava665k \
     --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter /mnt/bn/liuwenzhuo-lf/ckpt/llava-v1.5-7b-pretrain-ppio/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -17,16 +14,16 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-lora-debug-task2 \
+    --output_dir  /mnt/bn/liuwenzhuo-lf/ckpt/llava-v1.5-7b-finetune \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 2e-4 \
+    --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -37,7 +34,5 @@ deepspeed llava/train/train_mem.py \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb
-
-#/ppio_net0/code/openapi.sh stop 7dc5983214728bd5
 
 
