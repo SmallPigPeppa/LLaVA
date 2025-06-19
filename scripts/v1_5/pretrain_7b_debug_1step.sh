5b9d700bc650a835bd72f6c87500231cfc475ca2
lwz <905328134@qq.com>
2025-06-19 22:08:22 +0800

    initial

--- scripts/v1_5/pretrain_7b_debug_1step.sh
+++ scripts/v1_5/pretrain_7b_debug_1step.sh
@@ -12,7 +12,7 @@ deepspeed llava/train/train_mem.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir /mnt/bn/liuwenzhuo-lf/ckpt \
+    --output_dir /mnt/bn/liuwenzhuo-lf/ckpt/llava-v1.5-7b-pretrain-debug-1step \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 4 \
