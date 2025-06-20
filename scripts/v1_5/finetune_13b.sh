45882bfd205671e91baf97a312fba7bd7214d150
lwz <905328134@qq.com>
2025-06-20 21:09:33 +0800

    initial

--- scripts/v1_5/finetune_13b.sh
+++ scripts/v1_5/finetune_13b.sh
@@ -16,9 +16,9 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir  /mnt/bn/liuwenzhuo-lf/ckpt/llava-v1.5-13b-finetune \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
