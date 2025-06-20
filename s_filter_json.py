ffd3c93d3de60a858cef58338ce9f25c73594b1a
lwz <905328134@qq.com>
2025-06-20 12:39:06 +0800

    initial

--- s_filter_json.py
+++ s_filter_json.py
@@ -1,6 +1,7 @@
 import os
 import json
 from PIL import Image
+from lightning.app.storage.copier import num_workers
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -9,6 +10,7 @@ base_dir = '/mnt/hdfs/byte_content_security/user/liuwenzhuo/datasets/llava665k'
 input_path = os.path.join(base_dir, 'llava_v1_5_mix665k.json')
 output_path = os.path.expanduser('~/llava665k_validated.json')
 possible_extensions = ['.jpg', '.png', '.gif']
+num_workers=32
 
 def validate_item(item):
     original_image = item['image']
@@ -38,7 +40,7 @@ with open(input_path, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 validated = []
-with ThreadPoolExecutor(max_workers=32) as executor:
+with ThreadPoolExecutor(max_workers=num_workers) as executor:
     futures = {executor.submit(validate_item, item): item['id'] for item in data}
     for future in tqdm(as_completed(futures), total=len(futures), desc="Validating entries"):
         result = future.result()
