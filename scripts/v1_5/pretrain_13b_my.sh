d50a0da76b7a4234e17384b96c647b4eb08c1b6c
lwz <905328134@qq.com>
2025-06-24 14:58:30 +0800

    initial

--- scripts/v1_5/pretrain_13b_my.sh
+++ scripts/v1_5/pretrain_13b_my.sh
@@ -2,7 +2,7 @@
 MASTER_ADDR=127.0.0.1 MASTER_PORT=12345 \
 deepspeed llava/train/train_mem.py \
     --master_addr=localhost --master_port=12345 \
-    --deepspeed ./scripts/zero2.json \
+    --deepspeed ./scripts/zero2_debug.json \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --version plain \
     --data_path /mnt/hdfs/byte_content_security/user/liuwenzhuo/datasets/llava558k/blip_laion_cc_sbu_558k.json \
