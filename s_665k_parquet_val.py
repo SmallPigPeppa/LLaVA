061e151477914296f858e8e6e6109f4125ca5d96
lwz <905328134@qq.com>
2025-06-24 17:17:37 +0800

    initial

---
+++ s_665k_parquet_val.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+file_debug = '/mnt/bn/liuwenzhuo-hl-data/datasets/parquet/llava665k/debug.parquet'
+
+def read_and_print_one(path):
+    df = pd.read_parquet(path, engine='pyarrow')
+    print(f"=== File: {path} ===")
+    print("Columns:", df.columns.tolist())
+    print("First record:\n", df.iloc[0])
+    import pdb; pdb.set_trace()
+
+if __name__ == '__main__':
+    read_and_print_one(file_debug)
