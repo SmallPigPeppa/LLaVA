297e3c74ca0c3913069440e570565413acdf6b58
lwz <905328134@qq.com>
2025-06-25 21:00:02 +0800

    initial

--- scripts/v1_5/finetune_7b_mspe.sh
+++ scripts/v1_5/finetune_7b_mspe.sh
@@ -20,7 +20,7 @@ llava/train/train_mem_mspe.py \
     --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
