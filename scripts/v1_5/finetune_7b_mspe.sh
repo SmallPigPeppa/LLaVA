a14e5c453bdbac76a9d1227754239cf65a231ad3
lwz <905328134@qq.com>
2025-06-25 21:21:24 +0800

    initial

--- scripts/v1_5/finetune_7b_mspe.sh
+++ scripts/v1_5/finetune_7b_mspe.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#CUDA_VISIBLE_DEVICES=0  \
 deepspeed --master_addr=localhost --master_port=12345 \
 python llava/train/train_mem_mspe.py \
     --deepspeed ./scripts/zero3.json \
@@ -17,7 +16,7 @@ python llava/train/train_mem_mspe.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir  /mnt/bn/liuwenzhuo-lf/ckpt/llava-v1.5-7b-finetune \
+    --output_dir  /mnt/bn/liuwenzhuo-lf/ckpt/llava-v1.5-7b-finetune-mspe \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
